a35394a10722b6194e7992cdd8c2dc046e42419f
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2008-12-11 23:25:05 +0000

    Added the tab interface for the cube

--- c++/qtInterface/main.cpp
+++ c++/qtInterface/main.cpp
@@ -1,14 +1,22 @@
 #include <glew.h>
 #include <QApplication>
 #include "ui_main.h"
+#include "CubeDialog.h"
 
 class MainWindow: public QMainWindow, Ui::MainWindow
 {
 public:
   MainWindow() : QMainWindow(), Ui::MainWindow(){
     setupUi(this);
+    cubeDialog = new CubeDialog(tab);
+    cubeDialog->setObjectName(QString::fromUtf8("cubeDialog"));
+    cubeDialog->setGeometry(QRect(0, 20, 251, 381));
+    cubeDialog->setFrameShape(QFrame::StyledPanel);
+    cubeDialog->setFrameShadow(QFrame::Raised);
+    tabWidget->setTabText(tabWidget->indexOf(tab), QApplication::translate("MainWindow", "Cube", 0, QApplication::UnicodeUTF8));
   }
 
+  CubeDialog *cubeDialog;
 };
 
 
