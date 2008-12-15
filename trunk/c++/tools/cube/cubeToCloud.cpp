491eb68a62adeb398cbf4b3eb60353967cb06dd1
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2008-12-15 09:52:27 +0000

    implemented for point3D

--- trunk/c++/tools/cube/cubeToCloud.cpp
+++ trunk/c++/tools/cube/cubeToCloud.cpp
@@ -115,11 +115,86 @@ parse_opt (int key, char *arg, struct argp_state *state)
     default:
       return ARGP_ERR_UNKNOWN;
     }
-  return 0; 
+  return 0;
 }
 
 /* Our argp parser. */
 static struct argp argp = { options, parse_opt, args_doc, doc };
+
+enum Mode {c3D, c3Do, c3Dt, c3Dot} mode;
+
+
+// Given a cube, return a random point of it whose value is lower than 100
+vector< int > samplePoint(Cube< uchar, ulong>* cube, gsl_rng* r)
+{
+  // Fix depth
+  int x, y, z, nPointsLine, xRand, xCurr;
+  int nLinesVisited = 0;
+  int nLayersVisited = 0;
+  vector< int > toRet(3);
+  vector< bool > lines_visited(cube->cubeHeight);
+  for(int i=0; i < cube->cubeHeight; i++)
+    lines_visited[i] = false;
+  vector< bool > layers_visited(cube->cubeDepth);
+  for(int i=0; i < cube->cubeDepth; i++)
+    layers_visited[i] = false;
+
+  while( nLayersVisited <= cube->cubeDepth){
+    //Change this for the search of the layer
+    z = (int)(gsl_rng_uniform(r)*cube->cubeDepth);
+    if(layers_visited[z] == true) {
+      int zoffset = 0;
+      while( (layers_visited[ (z+zoffset)%cube->cubeDepth]== true) &&
+             (zoffset < cube->cubeDepth) ){
+        zoffset ++;
+      }
+      z = (z + zoffset)%cube->cubeDepth;
+    }
+    layers_visited[z] = true;
+    nLayersVisited++;
+    nLinesVisited = 0;
+    while( (nLinesVisited <= cube->cubeHeight) ){
+      y = (int)(gsl_rng_uniform(r)*cube->cubeHeight);
+      //Change this for the search of the line
+      if(lines_visited[y] == true) {
+        int yoffset = 0;
+        while( (lines_visited[ (y+yoffset)%cube->cubeHeight]== true) &&
+               (yoffset < cube->cubeHeight) ){
+          yoffset ++;
+        }
+        y = (y + yoffset)%cube->cubeHeight;
+      }
+      lines_visited[y] = true;
+      nLinesVisited++;
+      nPointsLine == 0;
+      for(int x = 0; x < cube->cubeWidth; x++){
+        if(cube->at(x,y,z) < 100){
+          nPointsLine++;
+        }
+      }
+      if(nPointsLine == 0) continue;
+      else {
+        xCurr = 0;
+        xRand = (int)(gsl_rng_uniform(r)*(nPointsLine+1));
+        for(int x = 0; x < cube->cubeWidth; x++){
+          if(cube->at(x,y,z) < 100){
+            xCurr++;
+            if(xCurr == xRand){
+              toRet[0] = x; toRet[1] = y; toRet[2] = z;
+              return toRet;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  //In case there is no point in the cube
+  toRet[0] = cube->cubeWidth; toRet[1] = cube->cubeHeight;
+  toRet[2] = cube->cubeDepth;
+  return toRet;
+}
+
 
 int main(int argc, char **argv) {
 
@@ -135,6 +210,8 @@ int main(int argc, char **argv) {
   args.number_points   = 0;
   args.number_negative_points = 0;
 
+  mode = c3D;
+
   argp_parse (&argp, argc, argv, 0, 0, &args);
 
   printf("Cube: %s\nTheta: %s\nPhi: %s\nOut: %s\nMsk: %s\nSave_negative: %i\nSave_type: %i\nSave_orientation: %i\nNumber_points: %i\nNumber_negative_points: %i\n",
@@ -145,29 +222,38 @@ int main(int argc, char **argv) {
          args.save_orientation, args.number_points,
          args.number_negative_points);
 
-  Cube_P* cborig = CubeFactory::load(args.name_cube);
-  if (cborig->type == "uchar")
-    cborig = dynamic_cast<Cube<uchar,ulong>*>(cborig);
-  else if (cborig->type == "float")
-    cborig = dynamic_cast<Cube<float,double>*>(cborig);
-  else{
-    printf("cubeToCloud: the cube is not of floats or uchars, exiting...\n");
-    exit(0);
-  }
+//   Cube_P* cborig = CubeFactory::load(args.name_cube);
+//   if (cborig->type == "uchar")
+//     cborig = dynamic_cast<Cube<uchar,ulong>*>(cborig);
+//   else if (cborig->type == "float")
+//     cborig = dynamic_cast<Cube<float,double>*>(cborig);
+//   else{
+//     printf("cubeToCloud: the cube is not of floats or uchars, exiting...\n");
+//     exit(0);
+//   }
+  Cube<uchar, ulong>* cborig = new Cube<uchar, ulong>(args.name_cube);
+  Cube<float, double>* theta;
+  Cube<float, double>* phi;
+  if( args.name_theta != "")
+    theta = new Cube<float, double>(args.name_theta);
+  if( args.name_phi != "")
+    phi = new Cube<float, double>(args.name_phi);
 
-  vector< int > indexes(3);
   vector< float > micrometers(3);
-  indexes[2] = 0;
 
-  Cloud_P* cloud;
-  if(args.save_orientation && args.save_type)
-    cloud = new Cloud< Point3Dot >();
-  if(args.save_orientation && !args.save_type)
-    cloud = new Cloud< Point3Do >();
-  if(!args.save_orientation && args.save_type)
-    cloud = new Cloud< Point3Dt >();
-  if(!args.save_orientation && !args.save_type)
-    cloud = new Cloud< Point3D >();
+//   Cloud_P* cloud;
+//   if(args.save_orientation && args.save_type){
+//     cloud = new Cloud< Point3Dot >();
+//     mode = c3Dot;}
+//   if(args.save_orientation && !args.save_type){
+//     cloud = new Cloud< Point3Do >();
+//     mode = c3Do;}
+//   if(!args.save_orientation && args.save_type){
+//     cloud = new Cloud< Point3Dt >();
+//     mode = c3Dt;}
+//   if(!args.save_orientation && !args.save_type){
+  Cloud<Point3D>* cloud = new Cloud< Point3D >();
+//     mode = c3D;}
 
   // Random number generation
   const gsl_rng_type * T2;
@@ -177,6 +263,16 @@ int main(int argc, char **argv) {
   r = gsl_rng_alloc (T2);
 
 
+  for(int i = 0; i < args.number_points; i++){
+    vector< int > indexes = samplePoint(cborig, r);
+    cborig->indexesToMicrometers(indexes, micrometers);
+    cloud->points.push_back(new Point3D(micrometers[0],
+                                        micrometers[1], micrometers[2]));
+
+  }
+
+
+  cloud->saveToFile(args.name_cloud);
 
 
 
