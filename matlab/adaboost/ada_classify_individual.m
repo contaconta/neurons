c848b1c43820879eb98cef5e474c6fc37faa1ac8
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-02-07 18:59:05 +0000

    added single_spedge

--- matlab/adaboost/ada_classify_individual.m
+++ matlab/adaboost/ada_classify_individual.m
@@ -25,59 +25,90 @@ for l = 1:length(LEARNERS)
     end
 
     if strcmp(LEARNERS(l).feature_type, 'spedge')
-        sp = spedges(I, LEARNERS(l).angles, LEARNERS(l).sigma);
-        %SP = sp.spedges(:);
+        edgelist.sigma = 0;
+        edgelist.EDGE = I;
     end
-    
 end
    
-
-
 % initialize C to zero
 C = 1;
 
     
 for s = 1:length(CASCADE)
-    
-    
     for l = 1:length(CASCADE(s).CLASSIFIER.weak_learners)
-        
         switch CASCADE(s).CLASSIFIER.weak_learners{l}.type
             
             case 'haar'
                 hinds = CASCADE(s).CLASSIFIER.weak_learners{l}.hinds;
                 hvals = CASCADE(s).CLASSIFIER.weak_learners{l}.hvals;
                 f(l) = ada_haar_response(hinds, hvals, II);
+
+                polarity(l) = CASCADE(s).CLASSIFIER.polarity(l);
+                theta(l) = CASCADE(s).CLASSIFIER.theta(l);
                 
+            case 'spedge'
+                angle = CASCADE(s).CLASSIFIER.weak_learners{l}.angle;
+                sigma = CASCADE(s).CLASSIFIER.weak_learners{l}.sigma;
+                row = CASCADE(s).CLASSIFIER.weak_learners{l}.row;
+                col = CASCADE(s).CLASSIFIER.weak_learners{l}.col;
+                
+                % if we've already computed EDGE for SIGMA, use it, otherwise use I
+                ind = find([edgelist(:).sigma] == sigma,1);
+                if ~isempty(ind)
+                    EDGE = edgelist(ind).EDGE;
+                    [f(l) EDGE] = single_spedge(angle, sigma, row, col, EDGE, 'edge');
+                    %disp('using a previously stored edge');
+                else
+                    [f(l) EDGE] = single_spedge(angle, sigma, row, col, I);
+                    %disp('using a newly computed edge');
+                end
+                
+                % store EDGE for this SIGMA value
+                if isempty(find([edgelist(:).sigma] == sigma,1));
+                    edgelist(length(edgelist)+1).sigma = sigma;
+                    edgelist(length(edgelist)).EDGE = EDGE;
+                end
+
+                polarity(l) = CASCADE(s).CLASSIFIER.polarity(l);
+                theta(l) = CASCADE(s).CLASSIFIER.theta(l);
+        end
+            
+        % weak classification
+        h(l) = polarity(l) * f(l) < polarity(l) * theta(l);
+
+    end
+    
+    % compute the strong classification
+    C = sum(h .* CASCADE(s).CLASSIFIER.alpha)  >  ( .5 * sum(CASCADE(s).CLASSIFIER.alpha) * CASCADE(s).threshold) ;
+        
+    % if the current stage generates a rejection, we can stop
+    if C == 0
+        break;
+    end
+end
+    
+
+
+%         sp = spedges(I, LEARNERS(l).angles, LEARNERS(l).sigma);
+
+%                 for a = 1:length(LEARNERS); if strcmp(LEARNERS(a).feature_type, CASCADE(s).CLASSIFIER.weak_learners{l}.type); L_ind = a; end; end;
+%                 angle_ind = find(LEARNERS(L_ind).angles ==CASCADE(s).CLASSIFIER.weak_learners{l}.angle);
+%                 sigma_ind = find(LEARNERS(L_ind).sigma ==CASCADE(s).CLASSIFIER.weak_learners{l}.sigma);
+%                 f(l) = sp.spedges(angle_ind, sigma_ind, CASCADE(s).CLASSIFIER.weak_learners{l}.row, CASCADE(s).CLASSIFIER.weak_learners{l}.col);                
+                
+
+
 %                 b(l) = TRAIN.responses.get(i, CASCADE(s).CLASSIFIER.weak_learners{l}.index);
 %                 
 %                 if abs(f(l) - b(l)) > 1e-03
 %                     keyboard;
 %                 end
-                
-                polarity(l) = CASCADE(s).CLASSIFIER.polarity(l);
-                theta(l) = CASCADE(s).CLASSIFIER.theta(l);
-                
-            case 'spedge'
-                %f = SP(CASCADE(s).CLASSIFIER.weak_learners{l}.index);
-                %angle = CASCADE(s).CLASSIFIER.weak_learners{l}.angle
-                for a = 1:length(LEARNERS); if strcmp(LEARNERS(a).feature_type, CASCADE(s).CLASSIFIER.weak_learners{l}.type); L_ind = a; end; end;
-                angle_ind = find(LEARNERS(L_ind).angles ==CASCADE(s).CLASSIFIER.weak_learners{l}.angle);
-                sigma_ind = find(LEARNERS(L_ind).sigma ==CASCADE(s).CLASSIFIER.weak_learners{l}.sigma);
-                f(l) = sp.spedges(angle_ind, sigma_ind, CASCADE(s).CLASSIFIER.weak_learners{l}.row, CASCADE(s).CLASSIFIER.weak_learners{l}.col);                
-                
-                polarity(l) = CASCADE(s).CLASSIFIER.polarity(l);
-                theta(l) = CASCADE(s).CLASSIFIER.theta(l);
-        end
-            
-        
-        % weak classification
-        h(l) = polarity(l) * f(l) < polarity(l) * theta(l);
         
         
 %         b(l) = polarity(l) * f(l) < polarity(l) * theta(l);
-    end
-    
+
+
+
 %     if ~isequal(h, b)
 %         disp('classifications based on computed features (f) and from bigmatrix (b) do not agree.');
 %         keyboard;
@@ -91,16 +122,3 @@ for s = 1:length(CASCADE)
 %     disp(['f        ' num2str(f)]);
 %     disp(['theta    ' num2str(theta)]);
 %     disp(['h        ' num2str(h)]);
-
-    
-    % compute the strong classification
-    C = sum(h .* CASCADE(s).CLASSIFIER.alpha)  >  ( .5 * sum(CASCADE(s).CLASSIFIER.alpha) * CASCADE(s).threshold) ;
-        
-    % if the current stage generates a rejection, we can stop
-    if C == 0
-        break;
-    end
-end
-    
-    
-
