bfbfece8a2615c62f24b1347eb5da51e2b62dfe1
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-02-16 01:05:09 +0000

    adjusted edgemethods

--- matlab/adaboost/ada_adaboost.m
+++ matlab/adaboost/ada_adaboost.m
@@ -1,13 +1,13 @@
 function CLASSIFIER = ada_adaboost(varargin)
 %% ADA_ADABOOST trains a strong classifier from weak classifiers & training data.
 %
-%   CLASSIFIER = ada_adaboost(PRE, TRAIN, WEAK, T) trains a strong
+%   CLASSIFIER = ada_adaboost(TRAIN, WEAK, ti, LEARNERS) trains a strong
 %   classifier CLASSIFIER from T hypotheses generated from weighted weak
 %   classifiers WEAK on training examples from data stored in 
-%   struct TRAIN.  T defines the number of hypotheses make up the strong 
+%   struct TRAIN.  ti defines the number of hypotheses make up the strong 
 %   classifier.  PRE is a bigmatrix containing precomputed feature responses 
 %   to the training set.  You may resume training an existing classifier by
-%   calling CLASSIFIER = ada_adaboost(PRE, TRAIN, WEAK, T, CLASSIFIER);.
+%   calling CLASSIFIER = ada_adaboost(PRE, TRAIN, WEAK, ti, CLASSIFIER);.
 %
 %
 %   Copyright © 2008 Kevin Smith
@@ -15,25 +15,33 @@ function CLASSIFIER = ada_adaboost(varargin)
 
 
 %% set parameters and handle input arguments
-TRAIN = varargin{1}; WEAK = varargin{2}; T = varargin{3}; LEARNERS = varargin{4};
+TRAIN = varargin{1}; WEAK = varargin{2}; ti = varargin{3}; LEARNERS = varargin{4};
 
 % either start or resume training, CLASSIFIER, w need to be passed or
 % initialized. start_t is the index of the first weak learner.
 if nargin == 4
     start_t = 1; w = ones(1,length(TRAIN.class)) ./ length(TRAIN.class); 
-    CLASSIFIER = ada_classifier_init(T, WEAK);
+    CLASSIFIER = ada_classifier_init(ti, WEAK);
 else
    CLASSIFIER = varargin{5};  
    start_t = length(CLASSIFIER.feature_index) + 1;  w = CLASSIFIER.w;
 end
 
 
-%% train the strong classifier as a series of T weak classifiers
-for t = start_t:T
+%% train the strong classifier as a series of ti weak classifiers
+for t = start_t:ti
     %% 1. Normalize the weights
     % normalize so each class has weight = 0.5
     w(TRAIN.class == 1) = .5 * (w(TRAIN.class==1) /sum(w(TRAIN.class==1)));
     w(TRAIN.class == 0) = .5 * (w(TRAIN.class==0) /sum(w(TRAIN.class==0)));
+    
+    %%================ DEBUGGING =======================================
+    if t == 100
+        disp('we are about to start repeating!');
+        keyboard;
+    end
+    %===================================================================
+    
     
     %% 2. train weak learners for optimal class separation
     WEAK = ada_train_weak_learners(WEAK, TRAIN, w);
@@ -80,78 +88,3 @@ for t = start_t:T
     clear IIs beta e f h    
 end
 
-
-
-
-
-
-    %WEAK = ada_train_weak_learners(WEAK, TRAIN, double(w));
-
- %weak_classifier             = WEAK.(WEAK.list{BEST_learner,1})(WEAK.list{BEST_learner,2});
-    %learner_ind                 = WEAK.list{BEST_learner,3};
-    %field                       = WEAK.learners{learner_ind}{1};
-    %classification_function     = WEAK.learners{learner_ind}{6};
-    %CLASSIFIER.learner_type{t}  = field;
-    
-%     % add the weak learner to the classifier (if it is not the first of its
-%     % type it needs to be appended, otherwise added)
-%     if ~isfield(CLASSIFIER, field)
-%         CLASSIFIER.weak_learners{1} = weak_classifier;
-%         CLASSIFIER.weak_learners{1}.alpha = alpha;
-%         CLASSIFIER.weak_learners{1}.index = BEST_learner;
-%         %CLASSIFIER.weak_learners{1}.type = field;
-%     else
-%         CLASSIFIER.weak_learners{t} = weak_classifier;
-%         CLASSIFIER.weak_learners{t}.alpha = alpha;
-%         CLASSIFIER.weak_learners{t}.index = BEST_learner;
-%         %CLASSIFIER.weak_learners{t}.type = weak_classifier.;
-%     end
-
-
-%     % add the weak classification function to a list of classification functions
-%     if isempty(CLASSIFIER.functions)
-%         CLASSIFIER.functions    = {field classification_function};
-%     elseif ~ismember(field, CLASSIFIER.functions(:,1))
-%         CLASSIFIER.functions    = [CLASSIFIER.functions ; {field classification_function}];
-%     end
-
-%     %%%%%%%%%%%%%%%%%%% DEBUG %%%%%%%%%%%%%%%%%%%%
-%     tmpstring =[];
-%     for i = 1:length(WEAK.learners)
-%         [m1, v1] = min(WEAK.error(WEAK.learners{i}{3}));
-%         s = ['best ' WEAK.learners{i}{1} ': ' num2str(v1) ' err=' num2str(m1) '   '];
-%         tmpstring = [tmpstring s];
-%     end
-%     disp(tmpstring);
-%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-%     % THIS WAS SUPPOSED TO COMPARE HAARS AND SPEDGES!
-%     %%%%%%%%%%%%%%%%%%% DEBUG %%%%%%%%%%%%%%%%%%%%
-%     for i = 1:length(WEAK.learners)
-%         [m1, v1] = min(WEAK.error(t));
-%         if m1 == min(WEAK.error); prestr = '     ✓ '; else prestr = '       '; end
-%         s = [prestr 'best ' WEAK.learners{i}.type ' error: ' num2str(m1) ', feature index: ' num2str(v1)];
-%         disp(s);
-%     end
-%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-%      %%%%%%%%%%%%%%%%%%% DEBUG %%%%%%%%%%%%%%%%%%%%
-%     for i = 1:length(WEAK.learners)
-%         [m1, v1] = min(WEAK.error(WEAK.learners{i}{3}));
-%         if m1 == min(WEAK.error); prestr = '     ✓ '; else prestr = '       '; end
-%         s = [prestr 'best ' WEAK.learners{i}{1} ' error: ' num2str(m1) ', feature index: ' num2str(v1)];
-%         disp(s);
-%     end
-%     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-
-    %disp(['...selected learner ' num2str(BEST_learner) ' (' field ') as t=' num2str(t) ]);
-    
-%     
-%     
-%     
-%     %% 4. Update the training weight vector according to misclassifications
-%     % get selected weak learner's classification results for the TRAIN set
-%     %h = classification_function(weak_classifier, TRAIN, [0 0], CLASSIFIER.IMSIZE);
-%     %h = classification_function(CLASSIFIER.feature_index(t), weak_classifier, TRAIN)';
