daaa550f07b57282deacf93512525ad55a78e523
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-02-25 17:53:21 +0000

    13-stage goals

--- matlab/adaboost/ada_stage_goals.m
+++ matlab/adaboost/ada_stage_goals.m
@@ -4,35 +4,31 @@
 % dmin      - default minimum detection rate for cascade stage
 clear GOALS;
 
-NStages = 1; %13;
+NStages = 13;
 
-GOALS(1).dmin = .90;
-GOALS(1).fmax = .00005;
+GOALS(1).dmin = .99;
+GOALS(1).fmax = .5;
+%---------------------------------------------------
+GOALS(2).dmin = .99;
+GOALS(2).fmax = .5;
+%---------------------------------------------------
+GOALS(3).dmin = .99;
+GOALS(3).fmax =  .45;
+%---------------------------------------------------
+GOALS(4).dmin = .991;
+GOALS(4).fmax =  .45;
+%---------------------------------------------------
+GOALS(5).dmin = .992;
+GOALS(5).fmax =  .4;
+%---------------------------------------------------
+GOALS(6).dmin = .993;
+GOALS(6).fmax =  .4;
+%---------------------------------------------------
 
-
-% GOALS(1).dmin = .99;
-% GOALS(1).fmax = .5;
-% %---------------------------------------------------
-% GOALS(2).dmin = .99;
-% GOALS(2).fmax = .5;
-% %---------------------------------------------------
-% GOALS(3).dmin = .99;
-% GOALS(3).fmax =  .45;
-% %---------------------------------------------------
-% GOALS(4).dmin = .991;
-% GOALS(4).fmax =  .45;
-% %---------------------------------------------------
-% GOALS(5).dmin = .992;
-% GOALS(5).fmax =  .4;
-% %---------------------------------------------------
-% GOALS(6).dmin = .993;
-% GOALS(6).fmax =  .4;
-% %---------------------------------------------------
-% 
-% S = 7;  Dcurrent = prod([GOALS(:).dmin]); Fcurrent = prod([GOALS(:).fmax]);
-% for s = S:NStages;
-%     GOALS(s).dmin = (Dtarget/Dcurrent)^(1/abs(NStages-S+1));
-%     GOALS(s).fmax = (Ftarget/Fcurrent)^(1/abs(NStages-S+1));
-% end
+S = 7;  Dcurrent = prod([GOALS(:).dmin]); Fcurrent = prod([GOALS(:).fmax]);
+for s = S:NStages;
+    GOALS(s).dmin = (Dtarget/Dcurrent)^(1/abs(NStages-S+1));
+    GOALS(s).fmax = (Ftarget/Fcurrent)^(1/abs(NStages-S+1));
+end
 
 clear S NStages;
