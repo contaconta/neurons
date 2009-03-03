edd1e4762b51e0280492b9029aac128f832fa6f7
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-03 02:23:44 +0000

    normalizing spdiff

--- matlab/adaboost/ada_spdiff_response.m
+++ matlab/adaboost/ada_spdiff_response.m
@@ -7,12 +7,14 @@ if nargin > 7
     EDGE = varargin{1};
     [d1 EDGE]   = single_spedge(angle1, stride, edge_method, row, col, EDGE, 'edge');
     [d2]        = single_spedge(angle2, stride, edge_method, row, col, EDGE, 'edge');
-    f = d1 - d2;
+    %f = d1 - d2;
+    f = (d1 - d2) / (d1 + 1);   % test to see if normalizing helps
 else
     I = varargin{1};
     [d1 EDGE]   = single_spedge(angle1, stride, edge_method, row, col, I);
     [d2]        = single_spedge(angle2, stride, edge_method, row, col, EDGE, 'edge');
-    f = d1 - d2;
+    %f = d1 - d2;
+    f = (d1 - d2) / (d1 + 1);   % test to see if normalizing helps
 end
 
 %keyboard;
