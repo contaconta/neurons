cccfa395cee7f76c867fc4612e7ad8da25171f28
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-03 02:23:44 +0000

    normalizing spdiff

--- trunk/matlab/adaboost/ada_precompute.m
+++ trunk/matlab/adaboost/ada_precompute.m
@@ -203,7 +203,8 @@ for l = 1:length(LEARNERS)
                 
                 SP = sp.spedges(:);
                 
-                R(j,:) = SP(ang1subs) - SP(ang2subs);
+                %R(j,:) = SP(ang1subs) - SP(ang2subs);
+                R(j,:) = (SP(ang1subs) - SP(ang2subs))./SP(ang1subs);   % test to see if normalzing helps
                 
 %                 % =======DEBUGGING - make sure my speed up stuff is computing
 %                 % features correctly.
