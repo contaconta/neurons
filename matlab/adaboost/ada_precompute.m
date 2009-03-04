8127335ef131bb28090fc1814e504c990734c6b1
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-04 02:17:42 +0000

    normalization back on

--- matlab/adaboost/ada_precompute.m
+++ matlab/adaboost/ada_precompute.m
@@ -205,9 +205,12 @@ for l = 1:length(LEARNERS)
                 sp = spedges(SET.Images(:,:,i), LEARNERS(l).angles, LEARNERS(l).stride, LEARNERS(l).edge_methods);
                 
                 SP = sp.spedges(:);
-                R(j,:) = SP(ang1subs) - SP(ang2subs);
-                %eta = .00001;
-                %R(j,:) = (SP(ang1subs) - SP(ang2subs))./ (SP(ang1subs) + eta*ones(size(SP(ang1subs))));   % test to see if normalzing helps
+                %R(j,:) = SP(ang1subs) - SP(ang2subs);
+                % =============FOR NORMALIZING THE DISTANCES ===================
+                eta = .00001;
+                R(j,:) = (SP(ang1subs) - SP(ang2subs))./ (SP(ang1subs) + eta*ones(size(SP(ang1subs))));   % test to see if normalzing helps
+                
+                
                 
 %                 % =======DEBUGGING - make sure my speed up stuff is computing
 %                 % features correctly.
