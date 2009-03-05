66a5647e917e7f5f8814ce625280b7671b8e1196
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-05 17:33:50 +0000

    Added for a point with weight

---
+++ c++/core/Point3Dotw.h
@@ -0,0 +1,36 @@
+#ifndef POINT3DOTW_H_
+#define POINT3DOTW_H_
+
+#include "neseg.h"
+
+#include <vector>
+#include <fstream>
+
+#include "Point3Dot.h"
+
+using namespace std;
+
+/** A point in 3D*/
+class Point3Dotw : public Point3Dot
+{
+public:
+
+  double weight;
+
+  Point3Dotw() : Point3Dot(){
+    weight = 0;
+  }
+
+  Point3Dotw(float x, float y, float z,  float theta=0, float phi = 0,
+             int type = -1, double _weight = w) :
+    Point3Dot(x,y,z,theta,phi,type)
+  {
+    weight = _weight;
+  }
+
+  static string className(){
+    return "Point3Dotw";
+  }
+};
+
+#endif
