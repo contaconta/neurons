7619b61af07c052366c00aa19e091478b029c177
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-05 16:51:49 +0000

    rays update

--- matlab/adaboost/ada_precompute.m
+++ matlab/adaboost/ada_precompute.m
@@ -205,10 +205,11 @@ for l = 1:length(LEARNERS)
                 sp = spedges(SET.Images(:,:,i), LEARNERS(l).angles, LEARNERS(l).stride, LEARNERS(l).edge_methods);
                 
                 SP = sp.spedges(:);
-                R(j,:) = SP(ang1subs) - SP(ang2subs);
+                
+                %R(j,:) = SP(ang1subs) - SP(ang2subs);
                 % =============FOR NORMALIZING THE DISTANCES ===================
-                %eta = .00001;
-                %R(j,:) = (SP(ang1subs) - SP(ang2subs))./ (SP(ang1subs) + eta*ones(size(SP(ang1subs))));   % test to see if normalzing helps
+                eta = .00001;
+                R(j,:) = (SP(ang1subs) - SP(ang2subs))./ (SP(ang1subs) + eta*ones(size(SP(ang1subs))));   % test to see if normalzing helps
                 
                 
                 
