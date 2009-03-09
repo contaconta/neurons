0c9770c0c37a818d71693d8f0cf78859961c20f8
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-09 19:50:05 +0000

    updated figures

---
+++ matlab/adaboost/ada_cut_cascade.m
@@ -0,0 +1,45 @@
+function CASCADE = ada_cut_cascade(CASCADE, nlearners)
+%
+%   CASCADE = ada_cut_cascade(CASCADE, nlearners)
+%
+%
+%
+%
+%
+
+
+learner_count = 0;
+
+for s = 1:length(CASCADE)
+    for i = 1:length(CASCADE(s).CLASSIFIER.weak_learners)
+
+        learner_count = learner_count + 1;
+        
+        if learner_count >= nlearners
+            disp('last learner found')
+            break;
+        end
+        
+    end
+    
+    if learner_count >= nlearners
+        disp('last learner found')
+        break;
+    end
+end
+
+
+CASCADE = CASCADE(1:s);
+CASCADE(s).CLASSIFIER.feature_index = CASCADE(s).CLASSIFIER.feature_index(1:i);
+CASCADE(s).CLASSIFIER.polarity = CASCADE(s).CLASSIFIER.polarity(1:i);
+CASCADE(s).CLASSIFIER.theta = CASCADE(s).CLASSIFIER.theta(1:i);
+CASCADE(s).CLASSIFIER.alpha = CASCADE(s).CLASSIFIER.alpha(1:i);
+
+% refill the weak_learners
+learners = CASCADE(s).CLASSIFIER.weak_learners;
+CASCADE(s).CLASSIFIER.weak_learners = {};
+
+for k = 1:i
+    CASCADE(s).CLASSIFIER.weak_learners{k} = learners{k};
+end
+
