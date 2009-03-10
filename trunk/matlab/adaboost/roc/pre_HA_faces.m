ad73d85e83f0c9a2aebf27058d9bb7a8274fb95e
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-10 02:20:55 +0000

    more precomputes

--- trunk/matlab/adaboost/roc/pre_HA_faces.m
+++ trunk/matlab/adaboost/roc/pre_HA_faces.m
@@ -17,7 +17,7 @@ TEST.class = TEST.class(1:N_POS+N_NEG);
 
 %% load the CASCADE we will be evaluating and CUT it to size
 %---------------------------------------------------------------------
-load HA-nucleirays1bMar0309-211918.mat;
+load HA-facescv8bMar052009-163516.mat;
 %load SP-facescv8aMar052009-180951.mat;
 %load COMBO-facesrays2Mar052009-175802.mat;
 nlearners = 500;
@@ -25,7 +25,7 @@ CASCADE = ada_cut_cascade(CASCADE, nlearners);
 
 
 % define a place to store the files
-filenm    = [pwd 'pre_HA_faces.mat'];
+filenm    = [pwd '/' 'pre_HA_faces.mat'];
 
 %% precompute the feature responses, and store them in FILES.test_filenm
 ada_cascade_precom(TEST, CASCADE, LEARNERS, filenm);
---
+++ trunk/matlab/adaboost/roc/process_roc.m
@@ -0,0 +1,64 @@
+%% load the CASCADE we will be evaluating and CUT it to size
+%---------------------------------------------------------------------
+load HA-facescv8bMar052009-163516.mat;
+%load SP-facescv8aMar052009-180951.mat;
+%load COMBO-facesrays2Mar052009-175802.mat;
+
+%load HA-nucleirays1bMar0309-211918.mat;
+%load HO-nuclei-rays2Mar0309-211959.mat;
+%load SP-nucleicv25Mar082009-053748.mat;
+%load SP-nucleirays4bMar052009-195316.mat;
+%load COMBO-nucleigbMar082009-051944.mat;   % ON GANDALF!
+
+%load HA-mito02-Mar-2009-00.37.16.mat;
+%load HO-mito-02-Mar-2009-00.38.37.mat
+%load SP-mitorays4aMar052009-175430.mat;
+%load COMBO-mitorays4Mar072009-231100.mat;
+
+
+nlearners = 500;  %1800 NUCLEI, 1000 MITO, 500 FACES  (252 cvlabpc25)
+CASCADE = ada_cut_cascade(CASCADE, nlearners);
+
+
+gt = zeros([1 101500]);
+gt(1:1500) = 1;
+
+%zeros([1 71000]);
+%gt(1:1000) = 1;
+
+
+%% load the precomputed features
+load pre_HA_faces.mat;
+%load pre_SP_faces.mat;
+%load pre_COMBO_faces.mat;
+
+%load pre_HA_nuclei.mat;
+%load pre_HO_nuclei.mat;
+%load pre_SP1_nuclei.mat;
+%load pre_SP2_nuclei.mat;
+
+
+
+%% build the ROC curve!
+roc_HA_faces = plot_roc(CASCADE, A, gt);
+%roc_SP_faces = plot_roc(CASCADE, A, gt);
+%roc_COMBO_faces = plot_roc(CASCADE, A, gt);
+%roc_HA_nuclei = plot_roc(CASCADE, A, gt);
+%roc_HO_nuclei = plot_roc(CASCADE, A, gt);
+%roc_SP1_nuclei = plot_roc(CASCADE, A, gt);
+
+save roc_HA_faces.mat roc_HA_faces;
+%save roc_SP_faces.mat roc_SP_faces;
+%save roc_COMBO_faces.mat roc_COMBO_faces;
+
+%save roc_HA_nuclei.mat roc_HA_nuclei;
+%save roc_HO_nuclei.mat roc_HO_nuclei;
+%save roc_SP1_nuclei.mat roc_SP1_nuclei;
+
+
+grid on;  hold on;
+xlabel('False Positive Rate');
+ylabel('Correct Detection Rate');
+%legend('Haar', 'HOG', 'Rays');
+%set(gca, 'XTick', [0 .00002:.00002:.0002);
+
