241262eca9236d1ed4d2881820a08a991aae3521
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-03-10 05:42:33 +0000

    more precomputes

--- trunk/matlab/adaboost/ada_spdiff_response.m
+++ trunk/matlab/adaboost/ada_spdiff_response.m
@@ -14,12 +14,12 @@ if nargin > 7
     EDGE = varargin{1};
     [d1 EDGE]   = single_spedge(angle1, stride, edge_method, row, col, LN, EDGE, 'edge');
     [d2]        = single_spedge(angle2, stride, edge_method, row, col, LN, EDGE, 'edge');
-    %f = d1 - d2;
-    f = (d1 - d2) / (d1 + eta);   % test to see if normalizing helps
+    f = d1 - d2;
+    %f = (d1 - d2) / (d1 + eta);   % test to see if normalizing helps
 else
     I = varargin{1};
     [d1 EDGE]   = single_spedge(angle1, stride, edge_method, row, col, LN, I);
     [d2]        = single_spedge(angle2, stride, edge_method, row, col, LN, EDGE, 'edge');
-    %f = d1 - d2;
-    f = (d1 - d2) / (d1 + eta);   % test to see if normalizing helps
+    f = d1 - d2;
+    %f = (d1 - d2) / (d1 + eta);   % test to see if normalizing helps
 end
