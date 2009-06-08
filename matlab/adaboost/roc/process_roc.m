32ef2381f218b56625cda509945bb64c552d1c5c
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-06-08 14:54:14 +0000

    added projectx

--- matlab/adaboost/roc/process_roc.m
+++ matlab/adaboost/roc/process_roc.m
@@ -4,7 +4,7 @@
 %load SP-facescv8aMar052009-180951.mat;
 %load COMBO-facesrays2Mar052009-175802.mat;
 
-%load HA-nucleirays1bMar0309-211918.mat;
+load HA-nucleirays1bMar0309-211918.mat;
 %load HO-nuclei-rays2Mar0309-211959.mat;
 %load SP-nucleicv25Mar082009-053748.mat;
 %load SP-nucleirays4bMar052009-195316.mat;
@@ -12,23 +12,24 @@
 
 %load HA-mito02-Mar-2009-00.37.16.mat;
 %load HO-mito-02-Mar-2009-00.38.37.mat
-load SP-mitorays4aMar052009-175430.mat;
+%load SP-mitorays4aMar052009-175430.mat;
 %load COMBO-mitorays4Mar072009-231100.mat;
 
 
+nlearners = 408;
 %nlearners = 500;
 %nlearners = 1800;  %1800 NUCLEI, 1000 MITO, 500 FACES  (252 cvlabpc25)
-nlearners = 1000;
+%nlearners = 1000;
 CASCADE = ada_cut_cascade(CASCADE, nlearners);
 
-gt = zeros([1 71200]);   % mito
-gt(1:1200) = 1;
+%gt = zeros([1 71200]);   % mito
+%gt(1:1200) = 1;
 
 %gt = zeros([1 101500]);   % faces
 %gt(1:1500) = 1;
 
-%gt = zeros([1 71000]);    % nuclei
-%gt(1:1000) = 1;
+gt = zeros([1 71000]);    % nuclei
+gt(1:1000) = 1;
 
 
 %% load the precomputed features
@@ -36,14 +37,14 @@ gt(1:1200) = 1;
 %load pre_SP_faces.mat;
 %load pre_COMBO_faces.mat;
 
-%load pre_HA_nuclei.mat;
+load pre_HA_nuclei.mat;
 %load pre_HO_nuclei.mat;
 %load pre_SP1_nuclei.mat;
 %load pre_SP2_nuclei.mat;
 
 %load pre_HA_mito.mat;
 %load pre_HO_mito.mat;
-load pre_SP_mito.mat;
+%load pre_SP_mito.mat;
 
 
 %% build the ROC curve!
@@ -51,18 +52,20 @@ load pre_SP_mito.mat;
 %roc_SP_faces = plot_roc(CASCADE, A, gt);
 %roc_COMBO_faces = plot_roc(CASCADE, A, gt);
 %roc_HA_nuclei = plot_roc(CASCADE, A, gt);
+roc_HA_nuclei408 = plot_roc(CASCADE, A, gt);
 %roc_HO_nuclei = plot_roc(CASCADE, A, gt);
 %roc_SP1_nuclei = plot_roc(CASCADE, A, gt);
 %roc_SP2_nuclei = plot_roc(CASCADE, A, gt);
 %roc_HA_mito = plot_roc(CASCADE, A, gt);
 %roc_HO_mito = plot_roc(CASCADE, A, gt);
-roc_SP_mito = plot_roc(CASCADE, A, gt);
+%roc_SP_mito = plot_roc(CASCADE, A, gt);
 
 %% save the results!
 %save roc_HA_faces.mat roc_HA_faces;
 %save roc_SP_faces.mat roc_SP_faces;
 %save roc_COMBO_faces.mat roc_COMBO_faces;
 
+save roc_HA_nuclei408.mat roc_HA_nuclei408;
 %save roc_HA_nuclei.mat roc_HA_nuclei;
 %save roc_HO_nuclei.mat roc_HO_nuclei;
 %save roc_SP1_nuclei.mat roc_SP1_nuclei;
@@ -70,7 +73,7 @@ roc_SP_mito = plot_roc(CASCADE, A, gt);
 
 %save roc_HA_mito.mat roc_HA_mito;
 %save roc_HO_mito.mat roc_HO_mito;
-save roc_SP_mito.mat roc_SP_mito;
+%save roc_SP_mito.mat roc_SP_mito;
 
 grid on;  hold on;
 xlabel('False Positive Rate');
--- matlab/spedges/spedges.m
+++ matlab/spedges/spedges.m
@@ -15,7 +15,7 @@ function A = spedges(I, angles, stride, edge_methods)
 %   -------------------------
 %   I = imread('cameraman.tif');
 %   angles = 0:30:330;
-%   SPEDGE = spedges(I,angles,2);
+%   SPEDGE = spedges(I,angles,2, [29:35]);
 %   imagesc(SPEDGE(3,:,:));  axis image;
 %
 %   Copyright © 2008 Kevin Smith
