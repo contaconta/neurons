1d3e1d392f86d9ae7bfac620f8179de1108da626
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-06-26 16:23:41 +0000

    added image.png to testing

--- trunk/matlab/projectx/testing/rectangleFeature.m
+++ trunk/matlab/projectx/testing/rectangleFeature.m
@@ -1,4 +1,4 @@
-img = imread('Images/img1.jpg');
+img = imread('image.png');
 img = img(:,:,1);
 
 %figure, imshow(img);
@@ -6,9 +6,9 @@ img = img(:,:,1);
 %position = wait(h)
 position = [136.0000  135.0000  125.0000  105.0000];
 % position has the form [xmin ymin width height]
-masked_img=img(position(2):position(2)+position(4), position(1):position(1)+position(3));
+%masked_img=img(position(2):position(2)+position(4), position(1):position(1)+position(3));
 %imshow(masked_img);
 
-learner_param{1} = 'HA_Wax1ay1bx2by2_Bax1ay2bx2by3'
-learner_param{2} = 'HA_Wax1ay1bx10by10_Bax1ay10bx10by20'
+learner_param{1} = 'HA_Wax0ay0bx1by1_Bax0ay1bx1by2'
+learner_param{2} = 'HA_Wax0ay0bx9by9_Bax0ay9bx9by19'
 r=mexRectangleFeature(img,learner_param)
