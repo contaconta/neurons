6d7d2b5a58f4dbd4263966f0e7d7c7f3ec0a74cc
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-06-26 10:20:59 +0000

    more updates to projectx

---
+++ matlab/projectx/src/buildmex.sh
@@ -0,0 +1,13 @@
+make
+GCC=/usr/bin/c++
+MEXPATH=/usr/local/matlab/extern/include/
+MEX_ARG=-cxx
+
+$GCC -fPIC -c -I$MEXPATH mexBoxIntegral.c
+$GCC -fPIC -c -I$MEXPATH mexIntegralImage.c
+$GCC -fPIC -c -I$MEXPATH mexRectangleFeature.c
+$GCC -fPIC -c -I$MEXPATH mexEnumerateLearners.c
+mex mexBoxIntegral.o CMakeFiles/IntegralImages.dir/integral.o CMakeFiles/IntegralImages.dir/loadImage.o CMakeFiles/IntegralImages.dir/utils.o -lgcc `pkg-config --libs opencv` $MEX_ARG
+mex mexIntegralImage.o CMakeFiles/IntegralImages.dir/integral.o CMakeFiles/IntegralImages.dir/loadImage.o CMakeFiles/IntegralImages.dir/utils.o -lgcc `pkg-config --libs opencv` $MEX_ARG
+mex mexRectangleFeature.o CMakeFiles/IntegralImages.dir/integral.o CMakeFiles/IntegralImages.dir/loadImage.o CMakeFiles/IntegralImages.dir/utils.o -lgcc `pkg-config --libs opencv` $MEX_ARG
+mex mexEnumerateLearners.o CMakeFiles/IntegralImages.dir/enumerateLearners.o CMakeFiles/IntegralImages.dir/integral.o CMakeFiles/IntegralImages.dir/loadImage.o CMakeFiles/IntegralImages.dir/utils.o -lgcc `pkg-config --libs opencv` $MEX_ARG
