248977afcaf9894a954b22ce4ab3fa18d1857a25
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-07-03 10:05:42 +0000

    more updates to projectx

---
+++ matlab/projectx/bin/p_RectangleFeature.m
@@ -0,0 +1,26 @@
+function  responses = p_RectangleFeature(imgs, feats)
+%P_ENUMERATELEARNERS
+%
+%   TODO: documentation
+%
+%   Examples:
+%   ----------------------
+%
+%   See also P_TRAIN, P_SETTINGS
+
+%   Copyright © 2009 Computer Vision Lab, 
+%   École Polytechnique Fédérale de Lausanne (EPFL), Switzerland.
+%   All rights reserved.
+%
+%   Authors:    Kevin Smith         http://cvlab.epfl.ch/~ksmith/
+%               Aurelien Lucchi     http://cvlab.epfl.ch/~lucchi/
+%
+%   This program is free software; you can redistribute it and/or modify it 
+%   under the terms of the GNU General Public License version 2 (or higher) 
+%   as published by the Free Software Foundation.
+%                                                                     
+% 	This program is distributed WITHOUT ANY WARRANTY; without even the 
+%   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+%   PURPOSE.  See the GNU General Public License for more details.
+
+responses = mexRectangleFeature(imgs, feats);
