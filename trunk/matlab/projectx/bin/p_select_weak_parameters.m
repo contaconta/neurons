0115d5cec73a1ef80c9b4ed8d051c21490991e3f
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-07-03 13:08:11 +0000

    more updates to projectx

--- trunk/matlab/projectx/bin/p_select_weak_parameters.m
+++ trunk/matlab/projectx/bin/p_select_weak_parameters.m
@@ -5,6 +5,9 @@ function [error, threshold, pol] = p_select_weak_parameters(learner, SET, w)
 %responses = p_dummy_feature_values(learner, SET);
 responses = p_RectangleFeature(SET.IntImages, {learner});
 
+% TODO: load responses from a precomputed file
+%responses = loadfunction(leaner_index, learner_type);
+
 err = zeros(size(responses)); 
 polarity = zeros(size(err)); 
 
---
+++ trunk/matlab/projectx/bin/p_storeResponse.m
@@ -0,0 +1,6 @@
+function p_storeResponse(index, learner_type, data)
+
+% 'HA'
+
+% call the mex function to store the data to the server
+mexStoreResponse(index, learner_type, data);
