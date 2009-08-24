7626918b217b41060cd3070342a1ce9de041772e
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-08-24 16:22:44 +0000

    added fragment features, fixed responses ROW/COLUMN vector bug

--- matlab/features/fragments/frag_normcorr.m
+++ matlab/features/fragments/frag_normcorr.m
@@ -21,25 +21,25 @@ function N = frag_normcorr(I, fragment, p)
 %   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
 %   PURPOSE.  See the GNU General Public License for more details.
 
-I = double(I); fragment = double(fragment);
-
-mI = mean(I(:));
-mf = mean(fragment(:));
-sI = max(0.0000001, std(I(:)));
-sf = max(0.0000001, std(fragment(:)));
-
-n = numel(I);
-
-%keyboard;
-
-if p==1
-    N = ( 1/(n-1) ) * sum(sum( ((I-mI).*(fragment-mf)) ./ (sI*sf) )) ;
-else
-    N = ( 1/(n-1) ) * sum(sum( (((I-mI).*(fragment-mf)).^p) ./ (sI*sf) )) ;
-end
+% I = double(I); fragment = double(fragment);
+% 
+% mI = mean(I(:));
+% mf = mean(fragment(:));
+% sI = max(0.0000001, std(I(:)));
+% sf = max(0.0000001, std(fragment(:)));
+% 
+% n = numel(I);
+% 
+% %keyboard;
+% 
+% if p==1
+%     N = ( 1/(n-1) ) * sum(sum( ((I-mI).*(fragment-mf)) ./ (sI*sf) )) ;
+% else
+%     N = ( 1/(n-1) ) * sum(sum( (((I-mI).*(fragment-mf)).^p) ./ (sI*sf) )) ;
+% end
 
 % TEMPORARILY CONVERT TO A INT32 BECAUSE MEMDAEMON CANNOT STORE DOUBLES!
 %N = int32( (N + 1) * (intmax('int32')/2));
 
 
-%N = sum(sum(I.*fragment));
+N = sum(sum(double(I).*double(fragment)));
