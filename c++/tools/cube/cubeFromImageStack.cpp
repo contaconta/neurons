dceaea8b97b13820fdb9fb5e0a5338a55cb3d446
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-08-24 09:53:39 +0000

    Changed to adapt it to Kevin's data

--- c++/tools/cube/cubeFromImageStack.cpp
+++ c++/tools/cube/cubeFromImageStack.cpp
@@ -24,18 +24,29 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-  if(argc!=10){
-    printf("Usage: cubeFromImageStack directory image_format nlayer_b nlayer_e voxel_width voxel_height, voxel_depth volume_name invert{0,1}\n");
+  if(!((argc!=10) || (argc!=11))){
+    printf("Usage: cubeFromImageStack directory image_format nlayer_b nlayer_e voxel_width voxel_height voxel_depth volume_name invert{0,1} kevinFormat{0,1}\n");
     exit(0);
   }
+
+  bool kevinFormat = false;
+  if(argc==11)
+    kevinFormat = atoi(argv[10]);
 
   printf("Directory %s\nImage fromat %s\n",
          argv[1], argv[2]);
 
   string directory = argv[1];
   Cube<uchar, ulong>* source = new Cube<uchar,ulong>();
-  source->create_cube_from_directory
-    (argv[1], argv[2], atoi(argv[3]),
-     atoi(argv[4]), atof(argv[5]), atof(argv[6]),
-     atof(argv[7]), argv[8], atoi(argv[9]));
+  if(!kevinFormat)
+    source->create_cube_from_directory
+      (argv[1], argv[2], atoi(argv[3]),
+       atoi(argv[4]), atof(argv[5]), atof(argv[6]),
+       atof(argv[7]), argv[8], atoi(argv[9]));
+  else
+    source->create_cube_from_kevin_images
+      (argv[1], argv[2], atoi(argv[3]),
+       atoi(argv[4]), atof(argv[5]), atof(argv[6]),
+       atof(argv[7]), argv[8]);
+
 }
