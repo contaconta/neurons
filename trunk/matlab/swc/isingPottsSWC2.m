a5ed71408c82722e2736ee01b0947f461cf197e7
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-09-20 19:12:49 +0000

    ising-Potts done -> some minor bugs remain

--- trunk/matlab/swc/isingPottsSWC2.m
+++ trunk/matlab/swc/isingPottsSWC2.m
@@ -1,50 +1,50 @@
-%% ========= parameters ==================
+% ========= parameters ==================
 S = 10000;
 
-% % set necessary paths
-% addpath([pwd '/utils/']);
-% addpath([pwd '/bin/']);
-% huttPath = '/osshare/Work/software/huttenlocher_segment/';
-% imPath = [pwd '/images/'];
+% set necessary paths
+addpath([pwd '/utils/']);
+addpath([pwd '/bin/']);
+huttPath = '/osshare/Work/software/huttenlocher_segment/';
+imPath = [pwd '/images/'];
+
+% fix the random number stream
+s = RandStream.create('mt19937ar','seed',5489);  RandStream.setDefaultStream(s);  %rand('twister', 100);    % seed for Matlab 7.8 (?)
+
+% load an image we want to play with
+Iraw = imread([imPath 'test.png']);
+
+% load superpixels or atomic regions as a label matrix, L
+HUT = imread([imPath 'testHUTT.ppm']);
+L = rgb2label(HUT);
+
+% G contains average gray levels of I for regions in L 
+Ig = label2gray(L,Iraw); Ig = uint8(round(Ig));
+
+% extract and adjacency matrix and list from L
+[G0, G0list] = adjacency(L);
+
+% create a list of superpixel center locations
+centers = zeros(max(L(:)),1);
+for l = 1:max(L(:))
+    pixelList = find(L == l); 
+    [r,c] = ind2sub(size(L), pixelList);
+    centers(l,1) = mean(r);
+    centers(l,2) = mean(c);
+end
+
+% % plot the original image
+% figure; imshow(Iraw);  axis image off; set(gca, 'Position', [0 0 1 1]);
 % 
-% % fix the random number stream
-% s = RandStream.create('mt19937ar','seed',5489);  RandStream.setDefaultStream(s);  %rand('twister', 100);    % seed for Matlab 7.8 (?)
+% % plot the segmentation with average gray levels
+% figure; imshow(Ig);  axis image off; set(gca, 'Position', [0 0 1 1]);
 % 
-% % load an image we want to play with
-% Iraw = imread([imPath 'test.png']);
-% 
-% % load superpixels or atomic regions as a label matrix, L
-% HUT = imread([imPath 'testHUTT.ppm']);
-% L = rgb2label(HUT);
-% 
-% % G contains average gray levels of I for regions in L 
-% Ig = label2gray(L,Iraw); Ig = uint8(round(Ig));
-% 
-% % extract and adjacency matrix and list from L
-% [G0, G0list] = adjacency(L);
-% 
-% % create a list of superpixel center locations
-% centers = zeros(max(L(:)),1);
-% for l = 1:max(L(:))
-%     pixelList = find(L == l); 
-%     [r,c] = ind2sub(size(L), pixelList);
-%     centers(l,1) = mean(r);
-%     centers(l,2) = mean(c);
-% end
-% 
-% % % plot the original image
-% % figure; imshow(Iraw);  axis image off; set(gca, 'Position', [0 0 1 1]);
-% % 
-% % % plot the segmentation with average gray levels
-% % figure; imshow(Ig);  axis image off; set(gca, 'Position', [0 0 1 1]);
-% % 
-% % % plot the superpixel centers
-% % figure; imshow(Ig);  axis image off; set(gca, 'Position', [0 0 1 1]);
-% % hold on; plot(centers(:,2), centers(:,1), 'b.');
-% 
-% % % plot the adjacency graph
-% % figure; imshow(Iraw); axis image off; set(gca, 'Position', [0 0 1 1]);
-% % hold on; gplot(G0, [centers(:,2) centers(:,1)], '.-'); 
+% % plot the superpixel centers
+% figure; imshow(Ig);  axis image off; set(gca, 'Position', [0 0 1 1]);
+% hold on; plot(centers(:,2), centers(:,1), 'b.');
+
+% % plot the adjacency graph
+% figure; imshow(Iraw); axis image off; set(gca, 'Position', [0 0 1 1]);
+% hold on; gplot(G0, [centers(:,2) centers(:,1)], '.-'); 
 
 
 B1 = .25;
