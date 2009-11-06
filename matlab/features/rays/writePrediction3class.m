650b04645267ecdabe38d4ee686f6a299ecb8771
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-11-06 12:02:19 +0000

    ultimate unary

---
+++ matlab/features/rays/writePrediction3class.m
@@ -0,0 +1,15 @@
+function writePrediction3class(path, filenm, predictions, L, label_types)
+
+[label_types, inds] = sort(label_types);
+
+fid = fopen([path filenm], 'w');
+
+fprintf(fid, 'labels %d %d\n', label_types(1), label_types(2), label_types(3));
+
+
+for s = 1:size(predictions,1)    
+    fprintf(fid, '%d %g %g %g\n', L(s), predictions(s, inds(1)), predictions(s, inds(2)), predictions(s, inds(3)));
+end
+
+
+fclose(fid);
