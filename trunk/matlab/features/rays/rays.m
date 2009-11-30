2e5e3b9ad9d4df10614b84b2f4e8bc97c618671e
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2009-11-30 18:20:48 +0000

    update to rays.m

--- trunk/matlab/features/rays/rays.m
+++ trunk/matlab/features/rays/rays.m
@@ -1,51 +1,80 @@
-function [RAY1 RAY3 RAY4] = rays(E, G, angle, stride)
-%RAYS computes RAY features
+function [RAY1 RAY3 RAY4] = rays(E, gv, gh, angle, varargin)
+%RAYS extracts RAY features [dist, orientation, norm]
 %
-%   FEATURE = spedge_dist(E, G, ANGLE, STRIDE)  computes a spedge 
-%   feature on a grayscale image I at angle ANGLE.  Each pixel in FEATURE 
-%   contains the distance to the nearest edge in direction ANGLE.  Edges 
-%   are computed using Laplacian of Gaussian zero-crossings (!!! in the 
-%   future we may add more methods for generating edges).  SIGMA specifies 
-%   the standard deviation of the edge filter.  
+%   [RAY1 RAY3 RAY4] = rays(E, GV, GH, angle) computes ray features in the
+%   direction given by ANGLE, given an input edge map E and the horizontal 
+%   gradient GH and vertical gradient GV. Outputs RAY1 contains type 1 Ray
+%   distance features, RAY3 contains type 3 orientation features, and RAY4
+%   contains type 4 norm features.  
 %
-%   Example:
-%   -------------------------
-%   I = imread('cameraman.tif');
+%   Example (compute Rays at 30 degrees):
+%   -------------------------------------
+%   I = imread('coins.png');
 %   gh = imfilter(I,fspecial('sobel')' /8,'replicate');
 %   gv = imfilter(I,fspecial('sobel')/8,'replicate');
-%   G(:,:,1) = gv;
-%   G(:,:,2) = gh;
-%   angle = 30;
-%   E = bwmorph(edge(I), 'diag');
-%   [Rdist Rori Rnorm] = rays(E, G, angle, 2);
-%   imagesc(SPEDGE);  axis image;
+%   E = bwmorph(edge(I, 'canny', .5,1), 'diag');
+%   [Rdist Rori Rnorm] = rays(E, gv, gh, 30); imagesc(Rdist); axis image;
 %
-%   Copyright © 2008 Kevin Smith
+%   Example (Rays extracted at multiple orientations):
+%   --------------------------------------------------
+%   angles = 0:30:330; 
+%   Rdist = zeros([size(I,1) size(I,2) length(angles)]);
+%   Rori = Rdist; Rnorm = Rdist;
+%   for i = 1:length(angles)
+%       [Rdist(:,:,i) Rori(:,:,i) Rnorm(:,:,i)] = rays(E, gv, gh, angles(i));
+%       imagesc(Rdist(:,:,i)); axis image; drawnow; refresh; 
+%   end
 %
-%   See also SPEDGES, EDGE, VIEW_SPEDGES
+%   Example (re-orient Rays to be rotationally invariant by shifting them 
+%            so orientation with longest ray becomes 0 degrees, SLOW):
+%   ------------------------------------------------------------------
+%   for r = 1:size(I,1)
+%       for c = 1:size(I,2)
+%           shift_places = -find(Rdist(r,c,:) == max(Rdist(r,c,:)),1)+1;
+%           a = 1:size(Rdist,3);
+%           a = circshift(a, [0 shift_places]);
+%           Rdist(r,c,:) = Rdist(r,c,a);
+%           Rori(r,c,:)  = Rori(r,c,a);
+%           Rnorm(r,c,:) = Rnorm(r,c,a);
+%       end
+%   end
+%
+%   Example (compute type 2 difference features from Type 1 features):
+%   ------------------------------------------------------------------
+%   pairs = combnk(angles, 2); eta = 1;
+%   for c = 1:size(pairs,1);
+%       angle1 = angles == pairs(c,1);
+%       angle2 = angles == pairs(c,2);
+%       Rdiff = (Rdist(:,:,angle1) - Rdist(:,:,angle2)) ./ (Rdist(:,:,angle1)+eta);
+%       Rdiff = exp(Rdiff);     % exponential difference can be more informative
+%       imagesc(Rdiff); axis image; drawnow; refresh; 
+%       if c == 6; disp('with rot inv: difference between longest ray and its opposite direction'); pause(3); end;
+%   end
+%
+%   Copyright © 2009 Kevin Smith
+%
+%   See also EDGE, IMFILTER, FSPECIAL
 
 
-
-
-% ensure good angles
+% ensure valid angles (between 0 and 360)
 angle = mod(angle,360);
 if angle < 0;  angle = angle + 360; end;
 
+% fill G with the vertical and horizontal gradient images
+G(:,:,1) = double(gv);  G(:,:,2) = double(gh);
+
 % compute the gradient norm GN
-G = double(G);
 GN = sqrt(sum((G.^2),3));
 
-% convert the Gradient G into unit vectors
-G = gradientnorm(G);
+% convert the gradient images in G into unit vectors
+G = gnormalize(G);
 
-
-
-% get a scanline in direction angle
+% get a scanline in direction 'angle'
 warning off MATLAB:nearlySingularMatrix; warning off MATLAB:singularMatrix;
-[Sr, Sc] = linepoints(E,angle);
+[Sr, Sc] = scanline(E,angle);
 warning on MATLAB:nearlySingularMatrix; warning on MATLAB:singularMatrix;
 
-% initialize the output matrices
+% allocate output matrices
 RAY1 = zeros(size(E));  % distrance rays
 RAY3 = zeros(size(E));  % gradient orientation
 RAY4 = zeros(size(E));  % gradient norm
@@ -53,9 +82,12 @@ RAY4 = zeros(size(E));  % gradient norm
 % determine the unit vector in the direction of the Ray
 rayVector = unitvector(angle);
 
+%% Main loop: compute rays along scanline, adjust scanline, iterate
+
 % if S touches the top & bottom of the image
 if ((angle >= 45) && (angle <= 135))  || ((angle >= 225) && (angle <= 315))
-    % SCAN TO THE LEFT!
+    
+    % SCAN LEFT
     j = 0;
     c = Sc + j;
     inimage = find(c > 0);
@@ -69,7 +101,6 @@ if ((angle >= 45) && (angle <= 135))  || ((angle >= 225) && (angle <= 315))
             if E(r(i),c(i)) == 1
                 steps_since_edge = 0;
                 lastGA = rayVector * [G(r(i),c(i),1); G(r(i),c(i),2)]; 
-                %if isnan(lastGA); keyboard; end;
                 lastGN = GN(r(i),c(i));
             end
             RAY1(r(i),c(i)) = steps_since_edge;
@@ -83,7 +114,7 @@ if ((angle >= 45) && (angle <= 135))  || ((angle >= 225) && (angle <= 315))
     end
 
 
-    % SCAN TO THE RIGHT!
+    % SCAN RIGHT
     j = 1;
     c = Sc + j;
     inimage = find(c <= size(E,2));
@@ -97,7 +128,6 @@ if ((angle >= 45) && (angle <= 135))  || ((angle >= 225) && (angle <= 315))
             if E(r(i),c(i)) == 1
                 steps_since_edge = 0;
                 lastGA = rayVector * [G(r(i),c(i),1); G(r(i),c(i),2)]; 
-                %if isnan(lastGA); keyboard; end;
                 lastGN = GN(r(i),c(i));
             end
             RAY1(r(i),c(i)) = steps_since_edge;
@@ -112,7 +142,7 @@ if ((angle >= 45) && (angle <= 135))  || ((angle >= 225) && (angle <= 315))
    
 % if S touches left & right of image (-pi/4 > angle > pi/4) or (3pi/4 > angle > 5pi/4)
 else
-    % SCAN TO THE bottom!
+    % SCAN TO BOTTOM
     j = 0;
     r = Sr + j;
     inimage = find(r > 0);
@@ -126,7 +156,6 @@ else
             if E(r(i),c(i)) == 1
                 steps_since_edge = 0;
                 lastGA = rayVector * [G(r(i),c(i),1); G(r(i),c(i),2)]; 
-                %if isnan(lastGA); keyboard; end;
                 lastGN = GN(r(i),c(i));
             end
             RAY1(r(i),c(i)) = steps_since_edge;
@@ -139,8 +168,7 @@ else
         inimage = find(r > 0);
     end
 
-
-    % SCAN TO THE top!
+    % SCAN TO TOP
     j = 1;
     r = Sr + j;
     inimage = find(r <= size(E,1));
@@ -154,7 +182,6 @@ else
             if E(r(i),c(i)) == 1
                 steps_since_edge = 0;
                 lastGA = rayVector * [G(r(i),c(i),1); G(r(i),c(i),2)]; 
-                %if isnan(lastGA); keyboard; end;
                 lastGN = GN(r(i),c(i));
             end
             RAY1(r(i),c(i)) = steps_since_edge;
@@ -168,7 +195,9 @@ else
     end
 end
 
-if stride ~= 1
+% specfying the stride will downsample the ray feature image
+if nargin > 4
+    stride = varargin{1};
     RAY1 = RAY1(1:stride:size(RAY1,1), 1:stride:size(RAY1,2));
     RAY3 = RAY3(1:stride:size(RAY3,1), 1:stride:size(RAY3,2));
     RAY4 = RAY4(1:stride:size(RAY4,1), 1:stride:size(RAY4,2));
@@ -176,15 +205,15 @@ end
 
 
 
+%% SUB-FUNCTIONS appear below ---------------------------------------------
 
 
-function [Sr, Sc] = linepoints(E,Angle)
-% defines the points in a line in an image at an arbitrary angle
-%
-%
-%
-%
-
+%% =============== SCANLINE ===============================================
+% [Sr, Sc] = scanline(E,Angle)
+%   Creates a prototype scanline in direction Angle, fit to the image. Sr
+%   constains the row coordinates of the scanline, Sc contains the column
+%   coordinates of the scanline.
+function [Sr, Sc] = scanline(E,Angle)
 
 % flip the sign of the angle (matlab y axis points down for images) and
 % convert to radians
@@ -197,7 +226,6 @@ end
 % format the angle so it is between 0 and less than pi/2
 if angle > pi; angle = angle - pi; end
 if angle == pi; angle = 0; end
-
 
 % find where the line intercepts the edge of the image.  draw a line to
 % this point from (1,1) if 0<=angle<=pi/2.  otherwise pi/2>angle>pi draw 
@@ -237,27 +265,21 @@ Sr = round(liney); Sc = round(linex);
 
 % if the angle points to quadrant 1 or 4, we need to re-sort the elements 
 % of Sr and Sc so they increase in the direction of the angle
-
 if (270 <= Angle) || (Angle < 90)
-    %reverse_inds = length(Sr):-1:1;
-    %Sr = Sr(reverse_inds);
-    %Sc = Sc(reverse_inds);
     Sr = flipud(Sr);
     Sc = flipud(Sc);
 end
 
 
-
-
-function [x,y] = intline(x1, x2, y1, y2)
-% intline creates a line between two points
-%INTLINE Integer-coordinate line drawing algorithm.
+%% ================== INTLINE =============================================
+% INTLINE Integer-coordinate line drawing algorithm.
 %   [X, Y] = INTLINE(X1, X2, Y1, Y2) computes an
 %   approximation to the line segment joining (X1, Y1) and
 %   (X2, Y2) with integer coordinates.  X1, X2, Y1, and Y2
 %   should be integers.  INTLINE is reversible; that is,
 %   INTLINE(X1, X2, Y1, Y2) produces the same results as
 %   FLIPUD(INTLINE(X2, X1, Y2, Y1)).
+function [x,y] = intline(x1, x2, y1, y2)
 
 dx = abs(x2 - x1);
 dy = abs(y2 - y1);
@@ -291,15 +313,43 @@ else
   y = (y1:y2).';
   x = round(x1 + m*(y - y1));
 end
-  
+
 if (flip)
   x = flipud(x);
   y = flipud(y);
 end
 
-
-function v = gradientnorm(v)
+%% ============== Gnormalize ============================================
+% N = gnormalize(G)
+%   transforms a NxMx2 array containing the x-gradient and y-gradient of an
+%   image to unit vectors.
+function v = gnormalize(v)
 v = double(v); eta = .00000001;
 mag = sqrt(  sum( (v.^2),3)) + eta;
 v = v ./ repmat(mag, [1 1 2]);
 
+%% ============== UNITVECTOR ==============================================
+% unitvec = unitvector(angle)
+% unitvec = unitvector(vector)
+%   u = unitvector(35);  % can accept angle arguments in degrees
+%   u = unitvector([2 5]);  % can accept vector arguments
+function unitvec = unitvector(data)
+
+% we have been given an angle
+if length(data) == 1   
+    unitvec(1) = sind(data);
+    unitvec(2) = cosd(data);
+% we have been given a vector    
+else 
+    % ensure it is a column vector
+    data = data(:);
+    if isequal(data, [0 0]');
+        % we this has no magnitude, pick a random angle
+        ang = rand(1)*2*pi;
+        unitvec(1) = sin(ang);
+        unitvec(2) = cos(ang);
+    else
+        % normalize the vector
+        unitvec = squeeze(l2norm(data));
+    end
+end
