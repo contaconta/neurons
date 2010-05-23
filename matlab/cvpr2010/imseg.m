f9687d320d267eaf7c31e9c68c3c5158562651db
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-05-23 14:40:40 +0000

    updates

--- matlab/cvpr2010/imseg.m
+++ matlab/cvpr2010/imseg.m
@@ -1,6 +1,9 @@
 %% IMSEG plots an image with its superpixel segmentation 
+%		
+%   Usage: [I, E] = imseg(I, L)  where I is the original image (uint8) and L contains
+%   the superpixel labels.  Returns I with superpixel boundaries drawn in gray, and E
+%   a map of the superpixel boundaries.
 %
-%   I = imseg(I, L)
 %   See also NLFILTER
 
 %   Copyright © 2009 Computer Vision Lab, 
@@ -18,7 +21,7 @@
 %   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
 %   PURPOSE.  See the GNU General Public License for more details.
 
-function I = imseg(I, L)
+function [I, E] = imseg(I, L)
 
 fun = @(x) ~sameval(x(:));
 E = nlfilter(L, [3 3], fun);
