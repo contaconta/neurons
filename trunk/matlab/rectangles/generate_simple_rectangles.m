7d862e4976d6b0672cda0a7dc9624e581b5a2349
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-06-02 23:05:38 +0000

    changes to rectangles

--- trunk/matlab/rectangles/generate_simple_rectangles.m
+++ trunk/matlab/rectangles/generate_simple_rectangles.m
@@ -4,7 +4,7 @@ rects = cell(N,1);
 cols = cell(N,1);
 
 for i = 1:N
-    RANKi = randi([2 RANK],1);
+    RANKi = randint([2 RANK],1);
     [rect, col] = generate_simple_rectangle(IMSIZE, RANKi);
     rects{i} = rect;
     cols{i} = col;
@@ -18,16 +18,16 @@ rects = cell(1,RANK); cols = zeros(1,RANK);
 for i = 1:RANK
     
     % sample the r and c coordinates
-    r = randi([1 24],1);
-    c = randi([1 24],1);
+    r = randint([1 24],1);
+    c = randint([1 24],1);
     
     % determine valid W and H
     WMAX = IMSIZE(2) - c + 1;
     HMAX = IMSIZE(1) - r + 1;
     
     % sample the w and h
-    w = randi([1 WMAX],1);
-    h = randi([1 HMAX],1);
+    w = randint([1 WMAX],1);
+    h = randint([1 HMAX],1);
  
     R = [r r r+h r+h];
     C = [c c+w c c+w];
