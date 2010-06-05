a9c877206225fa4ec39c4ec4b4f757da32a83cfc
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-06-05 18:50:34 +0000

    changes to rectangles

--- trunk/matlab/rectangles/rect_vis_ind.m
+++ trunk/matlab/rectangles/rect_vis_ind.m
@@ -1,7 +1,7 @@
-function blankI = rect_vis_ind(blankI,i,c,pol)
+function blankI = rect_vis_ind(blankI,r,c,pol)
 % visualize haar-like rectangles
 %
-% rect_vis_ind(blankI,i,c,pol)
+% rect_vis_ind(blankI,r,c,pol)
 %     blankI = zeros, size of window
 %     r = cell containing rectangles linear indexes
 %     c = matrix containing rectangle colors
@@ -15,16 +15,15 @@ end
 %figure(3436321);
 
 
-N = length(i);
+N = length(r);
 
 for n = 1:N
     
-    [rectR, rectC] = ind2sub(size(blankI)+[1 1], i{n});
-    %[rectC, rectR] = ind2sub(size(blankI)+[1 1], i{n});
+    [rectR, rectC] = ind2sub(size(blankI)+[1 1], r{n});
+    %[rectC, rectR] = ind2sub(size(blankI)+[1 1], r{n});
     %rectR = r{n};
     %rectC = c{n};
     col = c(n);
-
     
     %blankI(rectR(1):rectR(4)-1,rectC(1):rectC(4)-1) = pol*col;
     blankI(rectR(1):rectR(4)-1,rectC(1):rectC(4)-1) = blankI(rectR(1):rectR(4)-1,rectC(1):rectC(4)-1) + pol*col;
