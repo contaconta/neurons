c10afc2e3413ae4e4a7c8d444fb5c5211a962054
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-06-16 15:58:31 +0000

    changes to rectangles

--- trunk/matlab/rectangles/adaboost_settings.m
+++ trunk/matlab/rectangles/adaboost_settings.m
@@ -6,7 +6,7 @@ N_features = 2000;          % # of features to consider each boosting round
 N_pos = 5000;               % # of requested positive training examples
 N_total = 200000;           % # of total training examples
 N_SAMPLES = 25000;          % # of negative examples to use when choosing optimal learner parameters
-T = 20;                   % maximum rounds of boosting
+T = 2000;                   % maximum rounds of boosting
 CONNECTEDNESS = 0.8;        % probability rectangles will be connected
 EVAL = 0;                   % 1 = evaluate every boosting round/ 0 = no evaluation
 SE = 'francois';               % example sampling method: 'kevin' or 'francois'
@@ -20,7 +20,6 @@ mixrate = 0.25;             %'Simple', 'Kevin', 'VJSPECIAL', '33', '50',
 
 IMSIZE = [24 24];           % size of the classification window
 
-%host = hostname;                                       % compute hostname
 [s,host] = system('hostname'); host = strtrim(host);  	% computer hostname
 date = datestr(now, 'mmmddyyyy-HHMMSS');                % the current date & time
 
