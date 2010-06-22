c60addc3092684121c2bb12273160e32a061b4d2
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-06-22 17:41:19 +0000

    changes to rectangles

--- trunk/matlab/rectangles/generate_rectangles.m
+++ trunk/matlab/rectangles/generate_rectangles.m
@@ -23,15 +23,15 @@ for n = 1:N
         while isempty(rects)
             
             % first, sample the subwindow size
-            w = randi(24);
-            h = randi(24);
+            w = randint(24);
+            h = randint(24);
 
             % next sample an offset for the subwindow
-            ro = randi([0 IMSIZE(2) - w],1);
-            co = randi([0 IMSIZE(1) - h],1);
+            ro = randint([0 IMSIZE(2) - w],1);
+            co = randint([0 IMSIZE(1) - h],1);
 
             % sample the rank
-            rank = randi([2 RANK]);
+            rank = randint([2 RANK]);
             
             % generate a shape with sampled parameters
             rects = generate_shape1(w,h,rank,SIGMA,MAX_ASPECT);
