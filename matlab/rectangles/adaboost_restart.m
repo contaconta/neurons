1426d9624fadea80c4a50da672a52f539ac93807
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-06-23 16:47:53 +0000

    changes to rectangles

--- matlab/rectangles/adaboost_restart.m
+++ matlab/rectangles/adaboost_restart.m
@@ -1,85 +1,78 @@
 %% LOAD PARAMETERS
 adaboost_settings;
 
-%% LOAD CLASSIFIER
+
+%% PRE-BOOSTING
+tstart = tic;
+
+
+% LOAD CLASSIFIER
 filename = 'BaselineVJ-cvlabpc47-May242010-012203.mat';
 folder = [pwd '/results/'];
 load([folder filename]);
-if isfield(CLASSIFIER, 'pols')
-    CLASSIFIER.cols = CLASSIFIER.pols;
-    CLASSIFIER = rmfield(CLASSIFIER, 'pols');
-end
-if isfield(CLASSIFIER, 'tpol')
-    CLASSIFIER.pol = CLASSIFIER.tpol;
-    CLASSIFIER = rmfield(CLASSIFIER, 'tpol');
-end
 
-beta = zeros(T,1);      % computed beta value at each boosting step
-alpha = zeros(T,1);     % computed alpha value at each boosting step
-tstart = length(CLASSIFIER.rects);
-disp(['-------------- RESTARTING FROM t = ' num2str(tstart) ' --------------']);
-
-%% PRE-BOOSTING
-% pre-generate necessary feature pools
-if strcmp(RectMethod, 'Viola-Jones') || strcmp(RectMethod, 'Mixed50') || strcmp(RectMethod, 'Mixed33');
-    [R,C,N,P] = generate_viola_jones_features(IMSIZE); %[R,C,N,P] = generate_viola_jones_features(IMSIZE, 'shapes', {'horz2', 'vert2'});
-elseif strcmp(RectMethod, 'VJSPECIAL')
-    [R,C,N,P] = generate_viola_jones_features_special(IMSIZE);  % Rank3 has equal areas
-end
+% pre-generate necessary feature pools (VJ, Lienhart, ...)
+adaboost_pregenerate_features;
 
 % load the database into D
 adaboost_load_database;
 
-
 %% PERFORM BOOSTING
-for t = tstart:T
+
+[pathstr, name, ext, versn] = fileparts([results_folder EXP_NAME '-' host '-' date '.mat']);
+stats = zeros(T,7);     % keep statistics for each boosting round
+error = zeros(T,1);     % weighted classification error at each boosting step
+beta = zeros(T,1);      % computed beta value at each boosting step
+alpha = zeros(T,1);     % computed alpha value at each boosting step
+
+start_t = length(CLASSIFIER.rects);
+disp(['-------------- RESTARTING FROM t = ' num2str(tstart) ' --------------']);
+disp(' ');
+
+for t = start_t:T
     disp(' ');
     disp('===================================================================');
     disp(['              BOOSTING ROUND t = ' num2str(t) ',  ' EXP_NAME]);
     disp('-------------------------------------------------------------------');
     
     % randomly sample shape features
+   	clear f_areas f_rects f_cols f_types f_separea;
     get_random_shapes;
 
     % sample a subset of the whole training set to find optimal params
-    disp('...weighted sampling negative examples');
-    pos_inds = find(L == 1); neg_inds = find(L == -1);
-    neg_inds = weight_sample(neg_inds, W(neg_inds), N_SAMPLES);
-    inds = [pos_inds; neg_inds];
-    Lsub = L(inds);
-    Wsub = W(inds); Wsub(Lsub == -1) = Wsub(Lsub==-1) * ( sum(W(L==-1))/ sum(Wsub(Lsub==-1)));
-    Dsub = D(inds,:);
+    sample_examples;
     
     % populate the feature responses for the sampled features
-    disp('...computing feature responses for the selected features.'); tic;
-    F = zeros(size(Dsub,1), size(f_rects,1), 'single');
-    for i = 1:N_features
-        F(:,i) = haar_featureDynamicA(Dsub, f_rects{i}, f_cols{i}, f_areas{i});
-    end; to = toc; disp(['   Elapsed time (MATLAB) ' num2str(to) ' seconds.']);
-    %tic; Fmex = HaarFeature_mex(Dsub, f_rects(:)', f_cols(:)');
-    %to=toc;  disp(['   Elapsed time (MEX) ' num2str(to) ' seconds.']);
-    
-    
-    %% find the best weak learner
-    disp('...selecting the best weak learner and its parameters.');
-    tic; [thresh p e ind] = best_weak_learner(Wsub,Lsub,F);     % subset of training data
-    %tic; [thresh p e ind] = best_weak_learner(W,L,F);          % entire set of training data
-    
-    rect_vis_ind(zeros(IMSIZE), f_rects{ind}, f_cols{ind}, p); 
+    disp('...optimizing sampled features.'); tic;
+    if OPT_WEIGHTS
+        [thresh p e ind weights] = optimize_features_separea(Dsub, Wsub, Lsub, N_features, f_rects, brute_lists, f_separeas);
+    else
+        [thresh p e ind] = optimize_features(Dsub,Wsub,Lsub,N_features,f_rects,f_cols,f_areas,LOGR);
+    end
     to = toc; disp(['   Selected ' f_types{ind} ' feature ' num2str(ind) '. RANK = ' num2str(length(f_rects{ind})) ' thresh = ' num2str(thresh) '. Polarity = ' num2str(p) '. Time ' num2str(to) ' s.']);
+
+    % visualize the selected feature
+    rect_vis_ind(zeros(IMSIZE), f_rects{ind}, f_cols{ind}, p); 
+    
     
     %% compute error. sanity check: best weak learner should beat 50%
-    %E = p*F(:,ind) < p*thresh; E = single(E); E(E ==0) = -1; % prediction
-    E = AdaBoostClassifyDynamicA_mex(f_rects(ind), f_cols(ind), f_areas(ind),thresh, p, 1, D);
-%     if ANORM
-%         E = AdaBoostClassifyA(f_rects(ind), f_cols(ind), f_areas(ind),thresh, p, 1, D);
-%     else
-%         E = AdaBoostClassify(f_rects(ind), f_cols(ind), thresh, p, 1, D);
-%     end
+    % +class < thresh < -class for pol=1. -class < thresh < +class for pol=-1
+    
+    %F = haar_featureDynamicA(D, f_rects(ind), f_cols(ind), f_areas(ind));
+    %E = p * (F < thresh);
+    %keyboard;
+    if OPT_WEIGHTS
+        E = AdaBoostClassifyOPT_WEIGHTS_mex(f_rects(ind), {weights}, f_separeas(ind), thresh, p, 1, D);
+    elseif LOGR
+        E = AdaBoostClassifyDynamicA_LOGR(f_rects(ind), f_cols(ind), f_areas(ind),thresh, p, 1, D);
+    else
+        E = AdaBoostClassifyDynamicA_mex(f_rects(ind), f_cols(ind), f_areas(ind),thresh, p, 1, D);
+    end
     correct_classification = (E == L); incorrect_classification = (E ~= L);
     error(t) = sum(W .* incorrect_classification);
-    disp(['...local weighted error = ' num2str(e) ' global weighted error = ' num2str(error(t))]);
+    disp(['...sampled weighted error = ' num2str(e) ' global weighted error = ' num2str(error(t))]);
 
+    
     %% update the weights
     beta(t) = error(t) / (1 - error(t) );
     alpha(t) = log(1/beta(t));
@@ -96,15 +89,40 @@ for t = tstart:T
     CLASSIFIER.types{t} = f_types{ind};
     CLASSIFIER.norm = NORM;
     CLASSIFIER.method = RectMethod;
-
+    CLASSIFIER.dataset = DATASET;
+    if OPT_WEIGHTS
+        CLASSIFIER.separeas{t} = f_separeas{ind};
+        CLASSIFIER.weights{t} = weights;
+    end
+    
+    %keyboard;
+    
+    % TEMP AREA SANITY CHECK CODE
+    %[r c] = ind2sub([25 25], CLASSIFIER.rects{t}{1});
+    %a = compute_areas2([24 24], NORM, CLASSIFIER.rects(t), CLASSIFIER.cols(t));
+    %disp([  '   CLASSIFIER.areas=[' num2str(CLASSIFIER.areas{t})  '] a=[' num2str(a{1}) ']']);
+    
+    
+    
     % evaluate strong classifier performance, if desired (expensive)
     adaboost_eval;
    
     % store a temporary copy of boosted classifier
-    save([results_folder EXP_NAME '-' host '-' date '.mat'], 'CLASSIFIER', 'W', 'stats', 'error');
-    disp(['...saved as ' EXP_NAME '-' host '-' date '.mat']);
-    
-    % check for convergence (?)
-    
-%     keyboard;
+    save([pathstr '/' name ext], 'CLASSIFIER', 'W', 'stats', 'error'); disp(['...saved as ' name ext]);
+
 end
+
+
+%% evaluate the results on the test set!
+adaboost_self_evaluate([pathstr '/' name ext], EVALUATE_LIST);
+
+tstop = toc(tstart);
+cmd = ['mv ' pathstr '/' name ext ' ' pathstr '/finished/' name ext];
+system(cmd); 
+disp('==================================================================');
+disp(['  ' name ' finished processing in ' num2str(round(tstop)) ' seconds. Moved to /finished/']);
+disp('==================================================================');
+%send_gmail({'kevin.smith@epfl.ch'}, ['finished ' name], [host ' has completed ' name ' in ' num2str(round(tstop)) ' seconds.']);
+
+
+
