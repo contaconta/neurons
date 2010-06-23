efff288fbb74c5d212bd1c129dea4dff950f1cc3
cheeseburgerholdthecheese <cheeseburgerholdthecheese@3662888c-8ade-11dd-a205-0939b8ba2825>
2010-06-23 13:23:50 +0000

    changes to rectangles

--- trunk/matlab/rectangles/adaboost_self_evaluate.m
+++ trunk/matlab/rectangles/adaboost_self_evaluate.m
@@ -46,71 +46,77 @@ NTestSets = 11;
 L = [ ]; VALS = cell(1, length(Tlist));
 
 if sum(missing_list) ~= 0
-    for i = 1:NTestSets
+    ind1 = find(missing_list==1,1,'first');
+    if Tlist(1) <= length(CLASSIFIER.rects)
+    
+        for i = 1:NTestSets
 
-        % load part i of the data set
-        if strcmp(CLASSIFIER.method, 'Lienhart'); di_filename = ['45_TEST' num2str(i) '.mat'];
-        else; di_filename = ['TEST' num2str(i) '.mat']; end;    disp(['...loading ' di_filename]); %#ok<NOSEM>
-        load(di_filename);
+            % load part i of the data set
+            if strcmp(CLASSIFIER.method, 'Lienhart'); di_filename = ['45_TEST' num2str(i) '.mat'];
+            else; di_filename = ['TEST' num2str(i) '.mat']; end;    disp(['...loading ' di_filename]); %#ok<NOSEM>
+            load(di_filename);
 
-        % collect the labels
-        L = [L; Li]; %#ok<AGROW>
+            % collect the labels
+            L = [L; Li]; %#ok<AGROW>
 
-        fprintf('   classifying for T = ');
+            fprintf('   classifying for T = ');
+            for t = 1:length(Tlist)
+                if missing_list(t) == 1
+                    T = Tlist(t);
+                    if length(CLASSIFIER.rects) >= T
+                        rects = CLASSIFIER.rects(1:T);
+                        thresh = CLASSIFIER.thresh(1:T);
+                        alpha = CLASSIFIER.alpha(1:T);
+                        cols  = CLASSIFIER.cols(1:T);
+                        pol = CLASSIFIER.pol(1:T);
+                        areas = CLASSIFIER.areas(1:T);
+                        fprintf('%s ', num2str(T));
+                        if isfield(CLASSIFIER, 'separeas')
+                            separeas = CLASSIFIER.separeas(1:T);
+                            weights = CLASSIFIER.weights(1:T);
+                            VALSi = AdaBoostClassifyOPT_WEIGHTS_mex(rects, weights, separeas, thresh, pol, alpha, Di);
+                        else
+                            VALSi = AdaBoostClassifyDynamicA_mex(rects, cols, areas, thresh, pol, alpha, Di);
+                        end
+                        VALS{t} = [VALS{t}; VALSi];
+                    else
+                        fprintf('!not%s ', num2str(T)); 
+                    end
+                end
+            end
+            fprintf('\n');
+
+
+        end
+
+        % now that we have classification results, we need to evaluate the roc
+        % and interpolate!
+        NP = sum(L == 1);
+        NN = sum(L == -1);
+        fprintf('...evaluating the ROC for T = ');
         for t = 1:length(Tlist)
             if missing_list(t) == 1
-                T = Tlist(t);
-                if length(CLASSIFIER.rects) >= T
-                    rects = CLASSIFIER.rects(1:T);
-                    thresh = CLASSIFIER.thresh(1:T);
-                    alpha = CLASSIFIER.alpha(1:T);
-                    cols  = CLASSIFIER.cols(1:T);
-                    pol = CLASSIFIER.pol(1:T);
-                    areas = CLASSIFIER.areas(1:T);
-                    fprintf('%s ', num2str(T));
-                    if isfield(CLASSIFIER, 'separeas')
-                        separeas = CLASSIFIER.separeas(1:T);
-                        weights = CLASSIFIER.weights(1:T);
-                        VALSi = AdaBoostClassifyOPT_WEIGHTS_mex(rects, weights, separeas, thresh, pol, alpha, Di);
-                    else
-                        VALSi = AdaBoostClassifyDynamicA_mex(rects, cols, areas, thresh, pol, alpha, Di);
-                    end
-                    VALS{t} = [VALS{t}; VALSi];
+                if length(CLASSIFIER.rects) >= Tlist(t)
+                    fprintf('%s ', num2str(Tlist(t)));
+                    [TP FP] = roc_evaluate2(VALS{t}, L);
+                    TPR_list(t,:) = interp1(FP/NN,TP/NP,fplocs);
                 else
-                    fprintf('!not%s ', num2str(T)); 
+                    fprintf('skipping%s', num2str(Tlist(t)));
                 end
             end
         end
         fprintf('\n');
 
+        valid = Tlist <= length(CLASSIFIER.rects);
+        Tlist = Tlist(valid); %#ok<NASGU>
+        TPR_list = TPR_list(valid,:); %#ok<NASGU>
 
-    end
 
-    % now that we have classification results, we need to evaluate the roc
-    % and interpolate!
-    NP = sum(L == 1);
-    NN = sum(L == -1);
-    fprintf('...evaluating the ROC for T = ');
-    for t = 1:length(Tlist)
-        if missing_list(t) == 1
-            if length(CLASSIFIER.rects) >= Tlist(t)
-                fprintf('%s ', num2str(Tlist(t)));
-                [TP FP] = roc_evaluate2(VALS{t}, L);
-                TPR_list(t,:) = interp1(FP/NN,TP/NP,fplocs);
-            else
-                fprintf('skipping%s', num2str(Tlist(t)));
-            end
-        end
+        save([pathstr '/' name ext], 'CLASSIFIER', 'W', 'error', 'TPR_list', 'fplocs', 'Tlist');
+        disp(['... saved results in ' pathstr '/' name ext ]);
+    else
+        disp(['... not enough learners for T = [' num2str(Tlist(ind1)) ']']);
     end
-    fprintf('\n');
-    
-    valid = Tlist <= length(CLASSIFIER.rects);
-    Tlist = Tlist(valid); %#ok<NASGU>
-    TPR_list = TPR_list(valid,:); %#ok<NASGU>
-    
-    
-    save([pathstr '/' name ext], 'CLASSIFIER', 'W', 'error', 'TPR_list', 'fplocs', 'Tlist');
-    disp(['... saved results in ' pathstr '/' name ext ]);
 else
     disp(['... ' name ' already evaluated for T = [' num2str(Tlist) ']']);
 end
