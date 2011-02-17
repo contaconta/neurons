23ce870901b8e737c978a895cca6d73b910ee606
cheeseburgerholdthecheese@gmail.com <cheeseburgerholdthecheese@gmail.com@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-02-17 22:30:33 +0000

    up

--- trunk/matlab/iccv2011/determineKfromMask.m
+++ trunk/matlab/iccv2011/determineKfromMask.m
@@ -1,21 +1,27 @@
 function K = determineKfromMask(B, Kmin, Kmax, Afactor)
-
+% B(12,12) = 1;
+% B(12,13) = 1;
 
 Bwhite = B > 0;
 Bblack = B < 0;
-CCw = bwconncomp(Bwhite,4);
-%N = CCw.NumObjects;
-Pw = CCw.PixelIdxList;
-CCb = bwconncomp(Bblack,4);
-%N = CCw.NumObjects;
-Pb = CCb.PixelIdxList;
+w = regionprops(Bwhite, 'Area');
+b = regionprops(Bblack, 'Area');
 
-P = [Pw Pb];
+P = [w; b];
+
+% CCw = bwconncomp(Bwhite,4);
+% Pw = CCw.PixelIdxList;
+% CCb = bwconncomp(Bblack,4);
+% Pb = CCb.PixelIdxList;
+
+% P = [Pw Pb];
 %Kmin = N;
 
 atotal = 0;
+k = zeros(length(P),1);
 for i = 1:length(P)
-    areai = length(P{i});
+%     areai = length(P{i});
+    areai = P(i).Area;
     atotal = atotal + areai;
     
     k(i) = max(1,round(areai / Afactor));
