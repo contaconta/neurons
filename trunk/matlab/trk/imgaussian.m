9c40f6c2946559aa44207f0ac9e9acb397b4e852
cheeseburgerholdthecheese@gmail.com <cheeseburgerholdthecheese@gmail.com@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-02 17:43:21 +0000

    up

---
+++ trunk/matlab/trk/imgaussian.m
@@ -0,0 +1,41 @@
+function I=imgaussian(I,sigma,hsize)
+% IMGAUSSIAN filters an 1D, 2D color/greyscale or 3D image with an 
+% Gaussian filter. This function uses for filtering IMFILTER or if 
+% compiled the fast  mex code imgaussian.c . Instead of using a 
+% multidimensional gaussian kernel, it uses the fact that a Gaussian 
+% filter can be separated in 1D gaussian kernels.
+%
+% J=IMGAUSSIAN(I,SIGMA,SIZE)
+%
+% inputs,
+%   I: The 1D, 2D greyscale/color, or 3D input image with 
+%           data type Single or Double
+%   SIGMA: The sigma used for the Gaussian kernel
+%   SIZE: Kernel size (single value) (default: sigma*6)
+% 
+% outputs,
+%   J: The gaussian filtered image
+%
+% note, compile the code with: mex imgaussian.c -v
+%
+% example,
+%   I = im2double(imread('peppers.png'));
+%   figure, imshow(imgaussian(I,10));
+% 
+% Function is written by D.Kroon University of Twente (September 2009)
+
+%cls = class(I);
+I = double(I);
+hsize = [round(6*sigma + 1) 1];
+H = fspecial('gaussian',hsize, sigma);
+
+if(ndims(I)==1)
+    I = imfilter(I,H,'same', 'replicate');
+    %I = imfilter(I,H,'same', 'symmetric');
+elseif(ndims(I) ==2)
+    Hx = H';
+    Hy = H;
+    %I=imfilter(imfilter(I,Hy, 'replicate', 'conv'),Hx, 'replicate', 'conv');
+    %I=imfilter(imfilter(I,Hy, 'replicate'),Hx, 'replicate');
+    I=imfilter(imfilter(I,Hy, 'symmetric'),Hx, 'symmetric');
+end
