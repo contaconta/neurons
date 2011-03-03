6ec4ccb37efa91667ae747c50c2dca6affc1f349
cheeseburgerholdthecheese@gmail.com <cheeseburgerholdthecheese@gmail.com@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-03 00:28:00 +0000

    update

--- trunk/matlab/trk/trkGetDateAndLabel.m
+++ trunk/matlab/trk/trkGetDateAndLabel.m
@@ -47,6 +47,112 @@ switch exp_date
         else
             error('unknown experiment number!');
         end
+ 
+
+    case '15-11-2010'
+        if exp_label <= 10
+            label_txt = 'Not_kd';
+        elseif exp_label <= 20
+            label_txt = 'Not_Targ';
+        elseif exp_label <= 30
+            label_txt = 'RhoA_siRNA1';  
+        elseif exp_label <= 40
+            label_txt = 'RhoA_siRNA2';      
+        elseif exp_label <= 50
+            label_txt = 'RhoA_siRNA3'; 
+        elseif exp_label <= 60
+            label_txt = 'Net_siRNA';     
+        elseif exp_label <= 71
+            label_txt = 'Trio_siRNA2';       
+        elseif exp_label <= 81
+            label_txt = 'Trio_siRNA1';       
+        elseif exp_label <= 94
+            label_txt = 'SrGAP2_siRNA3';   
+        elseif exp_label <= 104
+            label_txt = 'SrGAP2_siRNA2';       
+        elseif exp_label <= 114
+            label_txt = 'Map2K7_siRNA1';      
+        elseif exp_label <= 124
+            label_txt = 'Map2K7_siRNA2'; 
+        elseif exp_label <= 134
+            label_txt = 'Map2K7_siRNA3';     
+        elseif exp_label <= 144
+            label_txt = 'SrGAP2_siRNA1'; 
+        else
+            error('unknown experiment number!');
+        end
+        
+        
+    case '16-11-2010'
+        if exp_label <= 10
+            label_txt = 'Not_kd';
+        elseif exp_label <= 19
+            label_txt = 'Not_Targ';
+        elseif exp_label <= 29
+            label_txt = 'RhoA_siRNA1';  
+        elseif exp_label <= 39
+            label_txt = 'RhoA_siRNA2';      
+        elseif exp_label <= 49
+            label_txt = 'RhoA_siRNA3'; 
+        elseif exp_label <= 59
+            label_txt = 'Net_siRNA';     
+        elseif exp_label <= 69
+            label_txt = 'Trio_siRNA2';       
+        elseif exp_label <= 78
+            label_txt = 'Trio_siRNA1';       
+        elseif exp_label <= 88
+            label_txt = 'SrGAP2_siRNA3';   
+        elseif exp_label <= 98
+            label_txt = 'SrGAP2_siRNA2';       
+        elseif exp_label <= 107
+            label_txt = 'Map2K7_siRNA1';      
+        elseif exp_label <= 117
+            label_txt = 'Map2K7_siRNA2'; 
+        elseif exp_label <= 127
+            label_txt = 'Map2K7_siRNA3';     
+        elseif exp_label <= 137
+            label_txt = 'SrGAP2_siRNA1'; 
+        else
+            error('unknown experiment number!');
+        end
+ 
+    case '17-11-2010'
+        if exp_label <= 10
+            label_txt = 'Not_kd';
+        elseif exp_label <= 20
+            label_txt = 'Not_Targ';
+        elseif exp_label <= 30
+            label_txt = 'RhoA_siRNA1';  
+        elseif exp_label <= 40
+            label_txt = 'RhoA_siRNA2';      
+        elseif exp_label <= 50
+            label_txt = 'RhoA_siRNA3'; 
+        elseif exp_label <= 60
+            label_txt = 'Net_siRNA';     
+        elseif exp_label <= 70
+            label_txt = 'Trio_siRNA2';       
+        elseif exp_label <= 80
+            label_txt = 'Trio_siRNA1';       
+        elseif exp_label <= 90
+            label_txt = 'SrGAP2_siRNA3';   
+        elseif exp_label <= 100
+            label_txt = 'SrGAP2_siRNA2';       
+        elseif exp_label <= 110
+            label_txt = 'Map2K7_siRNA1';      
+        elseif exp_label <= 120
+            label_txt = 'Map2K7_siRNA2'; 
+        elseif exp_label <= 130
+            label_txt = 'Map2K7_siRNA3';     
+        elseif exp_label <= 140
+            label_txt = 'SrGAP2_siRNA1'; 
+        else
+            error('unknown experiment number!');
+        end
+        
+        
+        
     otherwise
         error('unknown date!');
 end
+
+
