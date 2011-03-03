1af619ab0a492f2a9ef603823833d6507bf1a951
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-03 11:20:30 +0000

    made the code work on cvlabpc3

--- trunk/matlab/trk/trkAnalyze.m
+++ trunk/matlab/trk/trkAnalyze.m
@@ -1,21 +1,24 @@
 
+matlabpool
 
-
-folder = '/home/ksmith/data/Sinergia/Basel/14-11-2010/';
+folder = '/media/basel/Basel/14-11-2010/';
 
 d = dir(folder);
 
+addPath('code/');
+
 count = 1;
-for i = 1:140
+for i = 1:1
     exp_num(count,:) = sprintf('%03d', i); %#ok<SAGROW>
     count = count + 1;
 end
 
 for i = 1:size(exp_num,1)
-    
+    tic
     folder_n = [folder exp_num(i,:) '/'];
     trkTracking(folder_n);
     disp('');
     disp('=============================================================')
     disp('');
+    toc
 end
