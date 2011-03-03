f2b7fba665d882c52addb2cef338cbedba58aa8a
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-03 08:53:06 +0000

    removed verbosity added thresholds for p_min and p_max as to log(p_max)

--- trunk/matlab/trk/code/ComputeProbabilityMapShortestPath.cpp
+++ trunk/matlab/trk/code/ComputeProbabilityMapShortestPath.cpp
@@ -1,3 +1,17 @@
+/////////////////////////////////////////////////////////////////////////
+// This program is free software; you can redistribute it and/or       //
+// modify it under the terms of the GNU General Public License         //
+// version 2 as published by the Free Software Foundation.             //
+//                                                                     //
+// This program is distributed in the hope that it will be useful, but //
+// WITHOUT ANY WARRANTY; without even the implied warranty of          //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU   //
+// General Public License for more details.                            //
+//                                                                     //
+// Written and (C) by German Gonzalez                                  //
+// Contact <german.gonzalez@epfl.ch> for comments & bug reports        //
+/////////////////////////////////////////////////////////////////////////
+
 #include "mex.h"
 #include <boost/graph/graph_traits.hpp>
 #include <boost/graph/adjacency_list.hpp>
@@ -6,6 +20,24 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
+// 20110302
+/**
+   Function ComputeProbabilityMapShortestPath
+
+   Input:
+   - probability image:  (with values  between 0  and 1)
+
+   - mask: same  size as image, the valid values are:
+             - 0 the foreground pixels
+             - 1 places where no path should cross
+             - 2 source nodes
+
+   Output:
+   - Distance image. Each points represents the distance using Dijkstra shortest
+     path to the closest point in the mask labeled with 2.
+
+ */
 
 using namespace boost;
 
@@ -23,57 +55,53 @@ typedef graph_traits < GraphType >::vertex_descriptor VertexType;
 typedef graph_traits < GraphType >::edge_descriptor   EdgeType;
 typedef std::pair<long, long>                         Edge;
 
+#define EPS  0.0000000001
+#define EPS1 0.9999999999
 
 void mexFunction( int nlhs, mxArray *plhs[],
                   int nrhs, const mxArray *prhs[] )
 {
   double *Img,*Mask, *Output;
   mwSize nrows,ncols, nrows1, ncols1, nrows2, ncols2;
-  
+
   /* Check for proper number of arguments. */
   if(nrhs!=2) {
     mexErrMsgTxt("Two inputs required.");
   } else if(nlhs>2) {
     mexErrMsgTxt("Too many output arguments.");
   }
-  
+
   /* The input must be a noncomplex scalar double.*/
   nrows1 = mxGetM(prhs[0]);
   ncols1 = mxGetN(prhs[0]);
   if( !mxIsDouble(prhs[0]) || mxIsComplex(prhs[0]) ) {
     mexErrMsgTxt("Input 1 must be a noncomplex scalar double.");
   }
-  
+
   nrows2 = mxGetM(prhs[1]);
   ncols2 = mxGetN(prhs[1]);
   if( !mxIsDouble(prhs[1]) || mxIsComplex(prhs[1]) ) {
     mexErrMsgTxt("Input 2 must be a noncomplex scalar double.");
   }
-  
+
   if((nrows1!=nrows2)||(ncols1!=ncols2))
     mexErrMsgTxt("The images have different size");
-  
+
   nrows = nrows1;
   ncols = ncols1;
-  
+
   mexPrintf("Arguments correct\n");
-  
+
   /* Create matrix for the return argument. */
   plhs[0] = mxCreateDoubleMatrix(nrows,ncols, mxREAL);
-  
+
   /* Assign pointers to each input and output. */
   Img    = mxGetPr(prhs[0]);
   Mask   = mxGetPr(prhs[1]);
   Output = mxGetPr(plhs[0]);
 
-  int y = 127;
-  int x = 369;
-  mexPrintf("the value at [%i,%i] is %f and %f\n", x, y,
-            Img[y*ncols+x], Mask[y*ncols+x]);
-
-  
   mexPrintf("Creating the graph\n");
-  
+
   long num_nodes = (nrows)*(ncols);
   long num_arcs  = (nrows-1)*(ncols-1)*2; // Change afterwards to 8-connectivity
 
@@ -81,10 +109,10 @@ void mexFunction( int nlhs, mxArray *plhs[],
 
   std::vector< Edge > edge_array(num_arcs);
   std::vector< double > weights(num_arcs);
-  
+
   int rootX = 0;
   int rootY = 0;
-  
+
   long edgeCounter = 0;
   long ydx0, ydx1;
   double val;
@@ -93,34 +121,34 @@ void mexFunction( int nlhs, mxArray *plhs[],
     ydx1 = (y+1)*ncols;
     // printf("processing row %i\n", y);
     for(int x =0 ; x < ncols-1; x++){
-  
+
       if( (Mask[ydx0+x] == 1) && (Mask[ydx0+x+1] == 1) ) continue;
 
-      //     To the right              
+      //     To the right
       edge_array[edgeCounter] = Edge( ydx0 + x, ydx0 + x + 1);
       if( (Mask[ydx0+x] == 2) && (Mask[ydx0+x+1] == 2) )
-         val = 1;
+        val = 1;
       else
-         val = (Img[ydx0+x] + Img[ydx0+x+1])/2;
-      if(val > 0.9999999999) val = 0.9999999999;
-      if(val < 0.0000000001) val = 0.0000000001;
+        val = (Img[ydx0+x] + Img[ydx0+x+1])/2;
+      if(val > EPS1) val = EPS1;
+      if(val < EPS ) val = EPS;
       weights[edgeCounter]     = (double)(-log( val));
-      
+
       edgeCounter++;
 
       if( (Mask[ydx0+x] ==  1) && (Mask[ydx1+x] == 1) )
-      //     To the bottom
-      edge_array[edgeCounter] = Edge(ydx0+x, ydx1+x);
+        //     To the bottom
+        edge_array[edgeCounter] = Edge(ydx0+x, ydx1+x);
       if( (Mask[ydx0+x] ==  2) && (Mask[ydx1+x] == 2) )
-         val = 1;
+        val = 1;
       else
-      val = (Img[ydx0+x] + Img[ydx1+x])/2;
-      if(val > 0.9999999999) val = 0.9999999999;
-      if(val < 0.0000000001) val = 0.0000000001;
+        val = (Img[ydx0+x] + Img[ydx1+x])/2;
+      if(val > EPS1) val = EPS1;
+      if(val < EPS)  val = EPS;
       weights[edgeCounter]     = (double)(-log( val));
 
       edgeCounter++;
-      
+
       if(Mask[ydx0+x] > 0){
         rootX = x;
         rootY = y;
@@ -128,41 +156,42 @@ void mexFunction( int nlhs, mxArray *plhs[],
 
     }
   }
-  
-   
-   mexPrintf("Done creating the graph\n");
 
-   // Check for negative edge weights
+
+  mexPrintf("Done creating the graph\n");
+
+  // Check for negative edge weights
   for(unsigned int i = 0; i < num_arcs; i++)
-    if(weights[i] <= 0)
+    if(weights[i] <= 0){
+      weights[i] = EPS;
       printf("Edge[%i] = %f\n", i, weights[i]);
+    }
 
   GraphType g(&edge_array[0], &edge_array[0] + num_arcs, &weights[0], num_nodes);
 
-     
-    property_map<GraphType, edge_weight_t>::type weightmap = get(edge_weight, g);
+
+  property_map<GraphType, edge_weight_t>::type weightmap = get(edge_weight, g);
   std::vector<VertexType>    p(num_vertices(g));
   std::vector<double>        d(num_vertices(g));
 
 
 
-  
   VertexType s = vertex(rootY*ncols + rootX, g);
 
   dijkstra_shortest_paths(g, s, predecessor_map(&p[0]).distance_map(&d[0]));
 
 
   long rootIndex = rootY*ncols + rootX;
-  std::cout << "Distance to root: " << d[rootIndex] << " " << rootIndex << std::endl;
-  std::cout << " distance to the right " << d[rootIndex+1] << " parent: " << p[rootIndex+1] << std::endl;
-  std::cout << " distance to the left " << d[rootIndex-1] << " parent: " << p[rootIndex-1] <<std::endl;
+  // std::cout << "Distance to root: " << d[rootIndex] << " " << rootIndex << std::endl;
+  // std::cout << " distance to the right " << d[rootIndex+1] << " parent: " << p[rootIndex+1] << std::endl;
+  // std::cout << " distance to the left " << d[rootIndex-1] << " parent: " << p[rootIndex-1] <<std::endl;
 
   for(int y = 0; y < nrows; y++){
     for(int x = 0; x < ncols; x++){
-        Output[y*ncols + x] = exp(-d[y*ncols + x]);
+      Output[y*ncols + x] = exp(-d[y*ncols + x]);
     }
   }
-  
-  
-  
+
+
+
 }
