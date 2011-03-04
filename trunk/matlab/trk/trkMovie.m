433c2d98d8292ab2b6ac6225f3df9afa123d6704
cheeseburgerholdthecheese@gmail.com <cheeseburgerholdthecheese@gmail.com@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-04 02:53:05 +0000

    mencoder

--- trunk/matlab/trk/trkMovie.m
+++ trunk/matlab/trk/trkMovie.m
@@ -11,12 +11,21 @@ disp('...encoding movie');
 oldpath = pwd;
 cd(folder);
 %BITRATE = '5000';
-BITRATE = '25000000';
-FPS = '10';
-cmd = ['mencoder "mf://*.png" -mf fps=' FPS ' -o ' resultsFolder filename ' -ovc xvid -xvidencopts bitrate=' BITRATE ' -really-quiet'];
+%BITRATE = '25000000';
+%FPS = '10';
+%cmd = ['mencoder "mf://*.png" -mf fps=' FPS ' -o ' resultsFolder filename ' -ovc xvid -xvidencopts bitrate=' BITRATE ' -really-quiet'];
 % cmd = ['ffmpeg -r 10 -b 600k -i %03d.png ' resultsFolder filename];
 %disp(cmd);
-system(cmd);
+%system(cmd);
+
+BITRATE = 15000000;
+cmd1 = ['mencoder -ovc lavc -lavcopts vcodec=msmpeg4v2:vpass=1:"vbitrate=' num2str(BITRATE) ':mbd=2:keyint=132:vqblur=1.0:cmp=2:subcmp=2:dia=2:mv0:last_pred=3" -mf type=png:fps=10 -nosound -o /dev/null mf://*.png -really-quiet'];
+cmd2 = ['mencoder -ovc lavc -lavcopts vcodec=msmpeg4v2:vpass=2:"vbitrate=' num2str(BITRATE) ':mbd=2:keyint=132:vqblur=1.0:cmp=2:subcmp=2:dia=2:mv0:last_pred=3" -mf type=png:fps=10 -nosound -o ' resultsFolder filename ' mf://*.png'];
+
+system(cmd1);
+system(cmd2);
+
+
 cd(oldpath);
 
 cmd = ['rm ' folder '*.png'];
