4309d2705d4ed2b7f53b30c3a81854f6c6d2d43e
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-05 01:46:01 +0000

    deleted cleaned

--- trunk/matlab/trk/analysis/ffMeanGreenIntensity.m
+++
@@ -1,8 +0,0 @@
-function rvc = ffMeanGreenIntensity(R, varargin)
-
-rvc = zeros(length(R.D),1);
-
-for d = 1:length(R.D)
-   rvc(d) = R.D(d).MeanGreenIntensity;
-end
-
--- trunk/matlab/trk/analysis/ffSpeed.m
+++
@@ -1,9 +0,0 @@
-function rvc = ffSpeed(R, varargin)
-
-rvc = zeros(length(R.D),1);
-
-for d = 1:length(R.D)
-   d
-   rvc(d) = R.D(d).Speed;
-end
-
