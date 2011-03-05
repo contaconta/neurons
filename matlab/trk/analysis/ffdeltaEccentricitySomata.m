87bbf7b0537ae2501923d6d69cb46d1c4a7a79e1
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-05 01:47:00 +0000

    added the ffs for soma and nuclei

---
+++ matlab/trk/analysis/ffdeltaEccentricitySomata.m
@@ -0,0 +1,15 @@
+function rvc = ffdeltaEccentricitySomata(R, varargin)
+
+
+rvc = zeros(length(R.D),1);
+nPoints = 0;
+
+for d = 1:length(R.D)
+   if( isempty(R.D(d).ID) || R.D(d).ID==0)
+       continue;
+   end
+   nPoints = nPoints+1;
+   rvc(nPoints) = R.Soma(d).deltaEccentricity;
+end
+
+rvc = rvc(1:nPoints);
