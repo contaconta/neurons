52316c50282c50e54b805f1cbaf57b4d39a853a7
gerg1982@gmail.com <gerg1982@gmail.com@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-06 16:42:56 +0000

    added verbosity

--- trunk/matlab/trk/postProcessBaselMatResults.m
+++ trunk/matlab/trk/postProcessBaselMatResults.m
@@ -9,9 +9,11 @@ for nTrial =14:17
    parfor nRun = 1:200
      matFileName = sprintf('%i-11-2010_%03i.mat', nTrial, nRun)
      if( exist([dirIn matFileName]) > 0)
+       disp(matFileName);
         R = load([dirIn matFileName]);
         R = trkPostProcessing(R);
         save([dirOut matFileName], '-struct', 'R');
+       disp([matFileName ' done']);
      end
    end
    matlabpool close
--- trunk/matlab/trk/trk17.m
+++ trunk/matlab/trk/trk17.m
@@ -28,7 +28,7 @@ end
 
 
 count = 1;
-for i = 18:140
+for i = 14:14
     exp_num(count,:) = sprintf('%03d', i); %#ok<SAGROW>
     count = count + 1;
 end
