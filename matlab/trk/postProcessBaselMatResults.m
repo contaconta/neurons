8738b9a5e3a86a763b426066b32f31c1ceccc7ee
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-06 16:38:32 +0000

    changed matlab pool

--- matlab/trk/postProcessBaselMatResults.m
+++ matlab/trk/postProcessBaselMatResults.m
@@ -1,10 +1,11 @@
 
-matlabpool
+
 
 dirIn  = '/net/cvlabfiler1/home/ksmith/Basel/Results/';
 dirOut = '/net/cvlabfiler1/home/ksmith/Basel/PostResults/';
 
 for nTrial =14:17
+   matlabpool
    parfor nRun = 1:200
      matFileName = sprintf('%i-11-2010_%03i.mat', nTrial, nRun)
      if( exist([dirIn matFileName]) > 0)
@@ -13,6 +14,6 @@ for nTrial =14:17
         save([dirOut matFileName], '-struct', 'R');
      end
    end
+   matlabpool close
 end
 
-matlabpool close force
