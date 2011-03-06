00beecf1e4684e267413b2d4e59c9e70413baabb
gerg1982 <gerg1982@3662888c-8ade-11dd-a205-0939b8ba2825>
2011-03-06 19:20:28 +0000

    added trkTemporalAnalysisVector

---
+++ trunk/matlab/trk/trkTemporalAnalysisVector.m
@@ -0,0 +1,34 @@
+function [expContractVector, timeExpanding, timeContracting, numberInflexionPoints, freqCableExpansion] = trkTemporalAnalysisVector(v)
+
+
+expContractVector = zeros(size(v));
+
+numberInflexionPoints = 0;
+timeExpanding = 0;
+timeContracting = 0;
+freqCableExpansion = 0;
+
+for i = 1:length(v)-1
+    if(v(i+1) > v(i))
+        expContractVector(i) = 1;
+        timeExpanding = timeExpanding + 1;
+    else
+        expContractVector(i) = -1; 
+        timeContracting = timeContracting + 1;  
+    end
+end
+
+expContractVector(end) = expContractVector(end-1);
+if(expContractVector(end) == 1)
+    timeExpanding = timeExpanding+1;
+else
+    timeContracting = timeContracting + 1;  
+end
+
+for i = 1:length(v)-1
+    if(expContractVector(i) ~= expContractVector(i+1))
+        numberInflexionPoints = numberInflexionPoints+1;
+    end
+end
+
+freqCableExpansion = numberInflexionPoints/length(v);
