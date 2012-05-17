189f1ecd490fd5ebda9cdf32e57181369539bf82
fethallah <fethallah@gmail.com>
2012-05-17 15:49:48 +0000

    not enough colors svn st -u

--- matlab/basel/trkTracking.m
+++ matlab/basel/trkTracking.m
@@ -507,8 +507,8 @@ cols1 = jet(6);
 cols1 = cols1(randperm(6),:);
 cols2 = jet(8);
 cols2 = cols2(randperm(8),:);
-cols3 = jet(180);
-cols3 = cols3(randperm(180),:);
+cols3 = jet(250);
+cols3 = cols3(randperm(250),:);
 cols = [cols1; cols2; cols3];
 
 
