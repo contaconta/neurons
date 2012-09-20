a1618dc9d3421aa1b7706ed423774ea446cb66b2
fethallah <fethallah@3662888c-8ade-11dd-a205-0939b8ba2825>
2012-09-20 08:39:41 +0000

    Process plate and list of plates

---
+++ trunk/matlab/baselFinal/processListOfPlates.m
@@ -0,0 +1,38 @@
+function processListOfPlates(datasets_paths_filename)
+
+% processListOfPlates from a txt file under the format provided by
+% OpenBis 
+% 
+% This code is specific to the Sinergia project
+%
+% (c) Fethallah Benmansour, fethallah@gmail.com
+%
+%   Written 4/07/2012
+
+fid = fopen(datasets_paths_filename);
+C = textscan(fid, '%s %s %s %s %s');
+fclose(fid);
+
+inputDataRoot      = '/raid/data/store/';
+outputAnalisysRoot = '/raid/data/analysis/';
+
+for i= 1:length(C{1})
+    Sample	 = C{3}(i);
+    Identifier = C{4}(i);
+    Location = C{5}(i);
+    inputFolder = [inputDataRoot Location{1} '/original/'];
+    a = dir(inputFolder);
+    for j = 1:length(a)
+        if(a(j).isdir && strcmpi(a(j).name, Sample{1}))
+            directoryName = a(j).name;
+            break;
+        end
+    end
+    inputFolder = [inputFolder   directoryName '/' ];%#ok<*AGROW>
+    disp(inputFolder);
+    resultsFolder = [outputAnalisysRoot  Sample{1} '/'];
+    if( ~exist(resultsFolder, 'dir') )
+        mkdir(resultsFolder);
+    end
+    processPlate(inputFolder , resultsFolder, Sample{1}, Identifier{1});
+end
