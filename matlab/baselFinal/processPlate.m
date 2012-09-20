67b99afcd4254196ac79fd58ffd145de96e61f15
fethallah <fethallah@3662888c-8ade-11dd-a205-0939b8ba2825>
2012-09-20 08:39:41 +0000

    Process plate and list of plates

---
+++ matlab/baselFinal/processPlate.m
@@ -0,0 +1,74 @@
+function processPlate(folder, resultsFolder, Sample, Identifier)
+
+
+% processPlate: process one plate containing sub-directories
+% 
+% folder        : is the input folder
+% resultsFolder : the output folder where to write the results
+% Sample        : a string describing the experiment (or the plate, ex: PLATE3-G4)
+% Identifier    : a string Identifier for the experiment
+%
+% (c) Fethallah Benmansour, fethallah@gmail.com
+%
+%   Written 4/07/2012
+
+
+% ------------------- set the paths -----------------------
+if isempty( strfind(path, [pwd '/../baselFinal/frangi_filter_version2a']) )
+    addpath([pwd '/../baselFinal/frangi_filter_version2a']);
+end
+if isempty( strfind(path, [pwd '/../baselFinal/code']) )
+    addpath([pwd '/../baselFinal/code']);
+end
+if isempty( strfind(path, [pwd '/../baselFinal/gaimc']) )
+    addpath([pwd '/../baselFinal/gaimc']);
+end
+
+addpath('/home/fbenmans/src/neurons/matlab/baselFinal/RegionGrowing/');
+addpath('/home/fbenmans/src/neurons/matlab/baselFinal/ksp/');
+
+run('~/Downloads/vlfeat-0.9.14/toolbox/vl_setup');
+
+% --------- generate list of folders to process -----------
+count = 1;
+listOfDirs = dir(folder);
+for i = 1:length(listOfDirs)
+    if listOfDirs(i).isdir && length(listOfDirs(i).name) == 3
+        exp_num(count,:) = listOfDirs(i).name; %#ok<*AGROW>
+        count  = count + 1;
+    end
+end
+    
+
+filename_input = [resultsFolder 'OriginalDataDirectory.txt'];
+FID = fopen(filename_input, 'w');
+fprintf(FID, folder);
+fprintf(FID, '\n');
+fprintf(FID, Sample);
+fprintf(FID, '\n');
+fprintf(FID, Identifier);
+fclose(FID);
+
+
+
+% ------------ process the specified folders --------------
+% matlabpool local
+for i = 1:size(exp_num,1)
+    
+    folder_n = [folder exp_num(i,:) '/'];
+    G = trkTracking(folder_n, resultsFolder, exp_num(i,:), Sample);
+    a = dir([resultsFolder  exp_num(i,:) '.mat']);
+    disp('');
+    disp('=============================================================')
+    disp('');
+end
+
+%matlabpool close
+
+
+
+% kill the matlab pool
+%matlabpool close force
+
+
+
