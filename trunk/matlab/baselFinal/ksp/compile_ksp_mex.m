4d1f327afc7a3ca5d4ed915988404e8f8e47ae25
fethallah@gmail.com <fethallah@gmail.com@3662888c-8ade-11dd-a205-0939b8ba2825>
2013-01-16 16:50:53 +0000

    compilation files

--- trunk/matlab/baselFinal/ksp/compile_ksp_mex.m
+++ trunk/matlab/baselFinal/ksp/compile_ksp_mex.m
@@ -1,3 +1,5 @@
 system('make');
 
-mex -v  -cxx -I/usr/local/include/  ksp_graph.o ksp_computer.o ksp_matlab.o -o ksp_matlab 
+mex -v  -cxx -I/usr/local/include/  ksp_graph.o ksp_computer.o ksp_EMD_weights.o -o ksp_EMD_weights
+
+mex -v  -cxx -I/usr/local/include/  ksp_graph.o ksp_computer.o ksp_euclidean_dist_weights.o -o ksp_euclidean_dist_weights
