762cb404f7a4c6efc4e0de80113d0ecc163b8601
fethallah <fethallah@3662888c-8ade-11dd-a205-0939b8ba2825>
2013-03-07 17:21:40 +0000

    minor issue fixed: tracked neurites are effectivelly labeled as tracked

--- trunk/matlab/baselFinal2013/FeaturesExtraction/trkReorganizeDataStructure.m
+++ trunk/matlab/baselFinal2013/FeaturesExtraction/trkReorganizeDataStructure.m
@@ -75,23 +75,11 @@ for i = 1:length(trkNSeq)
             end
         end
         
-%         ListOfNeuritesInTrack(1).DelatTotalCableLength     = [];
-%         ListOfNeuritesInTrack(1).DeltaLengthBranchesMean   = [];
-%         ListOfNeuritesInTrack(1).DeltaMaxExtremeLength     = [];
-%         ListOfNeuritesInTrack(1).DeltaExtremeLengthMean    = [];
-%         ListOfNeuritesInTrack(1).DeltaMeanLeafLength       = [];
-%         ListOfNeuritesInTrack(1).DeltaNbBranches           = [];
-%         for j = 2:length(trkNSeq{i})
-%             deltaTime  = ListOfNeuritesInTrack(j).Time - ListOfNeuritesInTrack(j-1).Time;
-%             ListOfNeuritesInTrack(j).DelatTotalCableLength   = (ListOfNeuritesInTrack(j).TotalCableLength   - ListOfNeuritesInTrack(j-1).TotalCableLength) / deltaTime;%#ok
-%             ListOfNeuritesInTrack(j).DeltaLengthBranchesMean = (ListOfNeuritesInTrack(j).LengthBranchesMean - ListOfNeuritesInTrack(j-1).LengthBranchesMean) / deltaTime;%#ok
-%             ListOfNeuritesInTrack(j).DeltaMaxExtremeLength   = (ListOfNeuritesInTrack(j).MaxExtremeLength   - ListOfNeuritesInTrack(j-1).MaxExtremeLength) / deltaTime;%#ok
-%             ListOfNeuritesInTrack(j).DeltaExtremeLengthMean  = (ListOfNeuritesInTrack(j).ExtremeLengthMean  - ListOfNeuritesInTrack(j-1).ExtremeLengthMean) / deltaTime;%#ok
-%             ListOfNeuritesInTrack(j).DeltaMeanLeafLength     = (ListOfNeuritesInTrack(j).MeanLeafLength     - ListOfNeuritesInTrack(j-1).MeanLeafLength) / deltaTime;%#ok
-%             ListOfNeuritesInTrack(j).DeltaNbBranches         = (length(ListOfNeuritesInTrack(j).Branches)   - length(ListOfNeuritesInTrack(j-1).Branches)) / deltaTime;%#ok
-%         end
-        
         NeuritesTrack.Neurites     = ListOfNeuritesInTrack;
+        for j =1:length(NeuritesTrack.Neurites)
+            NeuritesTrack.Neurites(j).isTracked      = true;
+            NeuritesTrack.Neurites(j).NeuriteTrackId = NeuriteTrackId;
+        end
         NeuritesTrack.CellTrackIdx = CellTrackIdx;
         listOfNeuriteTracks{NeuriteTrackId} = NeuritesTrack;
         NeuriteTrackId = NeuriteTrackId + 1;
