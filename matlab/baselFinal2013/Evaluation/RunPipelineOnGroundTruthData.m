251f83bfef7e12203e0929d8fda50c8ff528902d
fethallah <fethallah@3662888c-8ade-11dd-a205-0939b8ba2825>
2013-07-16 14:39:34 +0000

    script to run the pipeline on the GT data

---
+++ matlab/baselFinal2013/Evaluation/RunPipelineOnGroundTruthData.m
@@ -0,0 +1,4 @@
+clear all; close all; clc;
+addpath('../')
+%%
+processPlate('/Users/feth/Google Drive/Sinergia/Selection10x/', '/Users/feth/Google Drive/Sinergia/Detections10x/', 'GT', '10x', '10x')
