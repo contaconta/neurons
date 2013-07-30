a7d9904b5cafe1306d9c2ccc14ff50c2421062f6
fethallah <fethallah@3662888c-8ade-11dd-a205-0939b8ba2825>
2013-07-30 13:29:11 +0000

    moving the SWC to OBJ conversion file up in the directory

---
+++ matlab/baselFinal2013/Evaluation/NeuriteDetectionEvaluation/DownSampleSWCFilesDir.m
@@ -0,0 +1,18 @@
+function [] = DownSampleSWCFilesDir(listOfInputDir, listOfDS_SWC)
+
+
+if exist(listOfDS_SWC, 'dir')
+    rmdir(listOfDS_SWC, 's');
+end
+
+mkdir(listOfDS_SWC);
+
+listOfSWCFiles = dir([listOfInputDir '*.swc']);
+
+for i =1:length(listOfSWCFiles)
+    cms_ds = ['DownSampleSWC/DownSample.exe -i "' listOfInputDir listOfSWCFiles(i).name '" -o "' listOfDS_SWC listOfSWCFiles(i).name '" -t 0.5'];
+    system(cms_ds);
+end
+
+
+
