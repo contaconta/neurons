b2829a726e90f5c52c6e8fd809e5200c1a6dbd61
fethallah <fethallah@3662888c-8ade-11dd-a205-0939b8ba2825>
2013-12-10 05:16:19 +0000

    fix the rare case where there's no connection between neurites at all

--- trunk/matlab/baselFinal2013/GreedyTracking/trkGreedyConnect2.m
+++ trunk/matlab/baselFinal2013/GreedyTracking/trkGreedyConnect2.m
@@ -29,79 +29,77 @@ Wr = tril(Wr);
 Wr(Wr == 0) = Inf;
 
 %keyboard;
-if isempty(Wr)
-    return;
-end
+if ~isempty(Wr)
+    while min_W < W_THRESH
 
-while min_W < W_THRESH
-    
-    % find the minimum weight
-    [min_W, min_ind] = min(Wr(:));
-    if isinf(min_W)
-        continue;
-    end
-    [rr,cr] = ind2sub(size(Wr), min_ind);
-    r = reducedToComplete(rr);
-    c = reducedToComplete(cr);
-    
-    % assign the connection in the tracking solution
-    T(r,c) = 1;
-    A(r,c) = 0;
-    Wr(completeToReduced(r),completeToReduced(c)) = Inf;
-    
-    % deteriming which is the future, which is the past
-    rt = D(r).Time;
-    ct = D(c).Time;
-    
+        % find the minimum weight
+        [min_W, min_ind] = min(Wr(:));
+        if isinf(min_W)
+            continue;
+        end
+        [rr,cr] = ind2sub(size(Wr), min_ind);
+        r = reducedToComplete(rr);
+        c = reducedToComplete(cr);
 
-    
-    if rt == ct
-        error('ERROR: equal time edges!');
-    end
-    
-    if rt < ct
-        past = r;  pt = rt;
-        fut = c;   ft = ct;
-    else
-        past = c;  pt = ct;
-        fut = r;   ft = rt;
-    end
-    
-    % remove all future connections for PAST
-    edgesr = find(A(past,:) == 1);
-    edgesc = find(A(:,past) == 1);
-    
-    for i = 1:length(edgesr)
-        if D(edgesr(i)).Time > pt;
-           A(past, edgesr(i)) = 0;
-              Wr(completeToReduced(past), completeToReduced(edgesr(i))) = Inf;
+        % assign the connection in the tracking solution
+        T(r,c) = 1;
+        A(r,c) = 0;
+        Wr(completeToReduced(r),completeToReduced(c)) = Inf;
 
+        % deteriming which is the future, which is the past
+        rt = D(r).Time;
+        ct = D(c).Time;
+
+
+
+        if rt == ct
+            error('ERROR: equal time edges!');
         end
-    end
-    for i = 1:length(edgesc)
-        if D(edgesc(i)).Time > pt;
-           A(edgesc(i), past) = 0;
-              Wr(completeToReduced(edgesc(i)), completeToReduced(past)) = Inf;
-         
+
+        if rt < ct
+            past = r;  pt = rt;
+            fut = c;   ft = ct;
+        else
+            past = c;  pt = ct;
+            fut = r;   ft = rt;
         end
-    end
-    
-    % remove all past connections for FUT
-    edgesr = find(A(fut,:) == 1);
-    edgesc = find(A(:,fut) == 1);
-    for i = 1:length(edgesr)
-        if D(edgesr(i)).Time < ft;
-           A(fut, edgesr(i)) = 0;
-              Wr(completeToReduced(fut), completeToReduced(edgesr(i))) = Inf;
-         
+
+        % remove all future connections for PAST
+        edgesr = find(A(past,:) == 1);
+        edgesc = find(A(:,past) == 1);
+
+        for i = 1:length(edgesr)
+            if D(edgesr(i)).Time > pt;
+               A(past, edgesr(i)) = 0;
+                  Wr(completeToReduced(past), completeToReduced(edgesr(i))) = Inf;
+
+            end
         end
-    end
-    for i = 1:length(edgesc)
-        if D(edgesc(i)).Time < ft;
-           A(edgesc(i), fut) = 0;
-              Wr(completeToReduced(edgesc(i)), completeToReduced(fut)) = Inf;
+        for i = 1:length(edgesc)
+            if D(edgesc(i)).Time > pt;
+               A(edgesc(i), past) = 0;
+                  Wr(completeToReduced(edgesc(i)), completeToReduced(past)) = Inf;
+
+            end
         end
+
+        % remove all past connections for FUT
+        edgesr = find(A(fut,:) == 1);
+        edgesc = find(A(:,fut) == 1);
+        for i = 1:length(edgesr)
+            if D(edgesr(i)).Time < ft;
+               A(fut, edgesr(i)) = 0;
+                  Wr(completeToReduced(fut), completeToReduced(edgesr(i))) = Inf;
+
+            end
+        end
+        for i = 1:length(edgesc)
+            if D(edgesc(i)).Time < ft;
+               A(edgesc(i), fut) = 0;
+                  Wr(completeToReduced(edgesc(i)), completeToReduced(fut)) = Inf;
+            end
+        end
+        %keyboard;
+
     end
-    %keyboard;
-    
 end
